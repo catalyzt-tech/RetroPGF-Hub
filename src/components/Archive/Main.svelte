df16a22aa22efcc0dd89a8b9637d3c44e62adede
Billy19191 <bill.ratchanon@gmail.com>
2023-08-10 23:57:47 +0700

    feat: nominated page + card component (draft)

---
+++ src/components/Archive/Main.svelte
@@ -0,0 +1,12 @@
+<script lang ='ts'>
+    import Card from "./Card.svelte";
+    import Data from "./results.json";
+</script>
+
+<h1 class="text-center font-bold mt-6 text-xl">Nominated Projects</h1>
+<p class="text-center">Read more about process and result in RetroPGF2 here</p>
+<div class="flex flex-wrap justify-center">
+    {#each Data as project}
+        <Card name={project['Project Name']} category={project['Category']} />
+    {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
 	import '../app.css';
 </script>
-
 <slot />
