ac8113bf8528e1817dd93e529d77fb9ea691ab79
Billy19191 <bill.ratchanon@gmail.com>
2023-08-10 23:57:47 +0700

    feat: nominated page + card component (draft)

---
+++ src/components/Archive/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let name: string;
+    export let category: string;
+  </script>
+  <div class="border-2 border-black w-1/5 m-5 p-4 rounded-2xl">
+    <h1 class="font-bold">{name}</h1>
+    <p>{category}</p>
+  </div>
+  
