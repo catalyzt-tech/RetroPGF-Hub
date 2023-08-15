22cef8d7feb1720b8ee0201a0eb8d7497ad00bc4
Billy19191 <bill.ratchanon@gmail.com>
2023-08-15 21:45:52 +0700

    feat: add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<div class="relative bg-[#FFE8E8] h-fit pt-5 text-sm">
+  <div class="absolute top-7 left-10">
+    <div class="flex space-x-3">
+      <a href="https://www.facebook.com" target="_blank">
+        <img src="/img/facebook_logo.png" class="w-7" alt="facebook" />
+      </a>
+      <a href="https://www.instagram.com" target="_blank">
+        <img src="/img/instagram_logo.png" class="w-7" alt="instagram" />
+      </a>
+      <a href="https://www.twitter.com" target="_blank">
+        <img src="/img/twitter_logo.png" class="w-7" alt="twitter" />
+      </a>
+    </div>
+  </div>
+  <span>
+    <div class="flex flex-row justify-end mx-10 mb-2 font-medium">
+      © Copyright 2023 RetroPGF Hub. All rights reserved.
+    </div>
+  </span>
+  <div class="flex flex-row justify-end mx-10 mb-4 text-[13px]">
+    <div>
+      This open-source project was built by <a
+        href="https://github.com/Billy19191"
+        target="_blank"
+        class="font-medium"
+      >
+        @Billy191
+      </a>
+      and
+      <a href="https://github.com/Wit03" target="_blank" class="font-medium"
+        >@Wit03</a
+      >
+    </div>
+  </div>
+</div>
