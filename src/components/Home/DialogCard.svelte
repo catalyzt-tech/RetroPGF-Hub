d64a6cca82c0bea914527daa1240fdb041457a9f
wit03 <witjarukit@gmail.com>
2023-08-17 02:28:41 +0700

    feat: some changes on homepage

---
+++ src/components/Home/DialogCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let title
+    let button
+</script>
+
+<div class="rounded-[40px] border-black border bg-white px-10 py-8 flex flex-col justify-center items-center">
+    <div>Mentors</div>
+    <div>Hello</div>
+    <div>button</div>
+
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,36 @@
 <script>
-  import Navbar from '../components/Navbar.svelte'
-  import Footer from '../components/Footer.svelte'
+  import Navbar from "../components/Navbar.svelte";
+  import Footer from "../components/Footer.svelte";
+  import Button from "../components/Button.svelte";
+  import DialogCard from "../components/Home/DialogCard.svelte";
 </script>
 
 <div>
   <Navbar />
   <!-- <img src="/img/bg.png" alt="op collective" class="w-screen" /> -->
 </div>
-<div class="bg-[#FFE8E8] w-full h-[40em]">
+<div
+  class="bg-[#FFE8E8] w-full h-[60vh] flex flex-col justify-center items-center"
+>
   <div class="pt-20 flex justify-center items-center">
-    <img src="img/sun.png" class="w-[15em]" alt="OP Sun" />
+    <img src="img/sun.png" class="w-[15em] animate-pulse" alt="OP Sun" />
   </div>
-
   <h1
-    class="flex flex-row pt-10 text-black text-[30px] font-bold justify-center"
+    class="flex flex-row pt-10 text-black text-[36px] text-center font-bold justify-center"
   >
-    Welcome to RetroPGF Hub!
+    Welcome to RetroPGF Hub
   </h1>
   <p class="flex flex-row justify-center text-lg p-2">
-    Nominate, Explore and Discover
+    Nominate, Get Ideas, and Discover
   </p>
   <div class="mt-3 flex flex-row justify-center">
-    <button
-      class="bg-black font-medium text-white py-3 px-8 rounded-[1em] text-lg"
-    >
-      Get involved</button
-    >
+    <Button>Get Involved!</Button>
   </div>
   <div class="flex justify-center mt-16 animate-bounce">
     <img src="/img/arrow_down2.png" class="w-6" alt="arrow" />
   </div>
 </div>
+<img class="w-screen" src="/img/bg.png" alt="op collective" />
 <div class="w-full h-[40em]">
   <div class="pt-6 flex flex-row text-[60px] justify-center italic">"</div>
   <h1 class="mb-6 flex flex-row justify-center text-[30px] italic font-medium">
@@ -42,5 +42,6 @@
     Let's Collaborate and Share Ideas: Together, We Shape Retroactive Public
     Good Funding.
   </p>
+  <DialogCard />
 </div>
 <Footer />
