a584ee8faeaff3aabc532bf5abfe9f031ad1868b
Billy19191 <bill.ratchanon@gmail.com>
2023-08-17 00:29:15 +0700

    feat: discovery page & large new card component

---
+++ src/routes/project-discovery/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Navbar from '../../components/Navbar.svelte'
+  import Footer from '../../components/Footer.svelte'
+  import BigCard from '../../components/BigCard.svelte'
+</script>
+
+<Navbar />
+<div>
+  <h1 class="text-[40px] flex justify-center font-bold">
+    Discovery Project nominated by the community
+  </h1>
+  <p class="text-md flex justify-center">Read more about RetroPGF3 here</p>
+  <div class="flex flex-wrap justify-center">
+    <BigCard />
+    <BigCard />
+    <BigCard />
+    <BigCard />
+  </div>
+</div>
+<Footer />
