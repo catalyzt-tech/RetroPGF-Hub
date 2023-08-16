ee3627375d6bf8006c329a1c723c2e8184ba6ddd
Billy19191 <bill.ratchanon@gmail.com>
2023-08-17 00:29:15 +0700

    feat: discovery page & large new card component

---
+++ src/components/BigCard.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div>
+  <div
+    class="relative border-2 border-black rounded-3xl w-[40em] h-72 p-6 my-6 mx-5 overflow-hidden"
+  >
+    <div class="absolute w-52 rounded-lg overflow-hidden top-8">
+      <img src="img/test_logo.png" alt="Logo" />
+    </div>
+    <div class="absolute left-64 text-[20px] font-bold">TokenUnlock</div>
+    <div class="absolute left-64 top-[4.5rem] line-clamp-4 w-[25em] text-sm">
+      Token unlocks refer to the release of previously locked or restricted
+      tokens into the market. These tokens become available for trading, buying,
+      and selling after the end of their vesting period. The vesting period is
+      the duration during which the tokens are restricted.
+    </div>
+    <div
+      class="absolute bottom-11 left-64 bg-gradient-to-r from-[#000000] to-[#DA0000] rounded-2xl px-6 py-3 w-[14em] text-white font-bold text-sm"
+    >
+      End User Experience & Adoption
+    </div>
+    <div class="absolute bottom-16 right-20 font-medium text-sm">
+      View More &gt
+    </div>
+  </div>
+</div>
