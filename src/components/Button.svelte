49ee93ffca8e8566ae824b7c2d9ac0cd65b4a17d
wit03 <witjarukit@gmail.com>
2023-08-17 02:26:45 +0700

    feat: button comp.

---
+++ src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+    let buttonProps = {
+        class: [$$restProps.class],
+    };
+</script>
+
+<button
+    on:click
+    on:focus
+    on:mouseenter
+    on:mouseleave
+    {...buttonProps}
+    class="bg-black font-medium text-white py-3 px-8 rounded-[1em] text-lg"
+>
+    <slot />
+</button>
