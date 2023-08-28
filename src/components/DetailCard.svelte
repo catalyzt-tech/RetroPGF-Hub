4c4c86d74225d80cd14cb6c1788015511765c66b
Billy19191 <bill.ratchanon@gmail.com>
2023-08-28 18:30:19 +0700

    feat: add new dataset (detail)

---
+++ src/components/DetailCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let detail: any
+</script>
+
+<div class="flex justify-center">
+  <div class=" border-2 rounded-3xl p-10 border-black w-[50em] h-fit m-20">
+    <div class="text-center text-xl font-bold">{detail?.name}</div>
+    <div class="font-bold">About the project</div>
+    <div>{detail?.about}</div>
+    <div class="font-bold">Website</div>
+    <div class="font-bold">Funding allocation</div>
+    <div class="font-bold">
+      How do you support development and usage of the OP Stack? What public good
+      do you provide to the Collective?
+    </div>
+    <div>{detail?.question_1}</div>
+    <div class="font-bold">
+      How do you sustain yourself? Please list sources of funding and revenue
+    </div>
+    <div>Recipient Address</div>
+  </div>
+</div>
--- src/routes/archive/[slug]/+page.js
+++ src/routes/archive/[slug]/+page.js
@@ -1,10 +1,17 @@
-import Data from '../../../data/results.json'
+import fs from 'fs'
 export const load = ({ params }) => {
-  console.log(params + 'and' + params.slug)
-  const fetchProject = Data.find(
-    (project) => project['Project Name'] === params.slug
-  )
-
-  return { detail: fetchProject }
+  try {
+    // console.log(params + 'and' + params.slug)
+    const data = fs.readFileSync(
+      `public/data/retroPGF2-dataset/each_project/${params.slug}/info.json`
+    )
+    console.log(data)
+    const fetchProjectTemp = data.toString()
+    const fetchProject = JSON.parse(fetchProjectTemp)
+    console.log(fetchProject)
+    return { detail: fetchProject }
+  } catch (err) {
+    console.log(err)
+  }
 }
 //param = object, .slug is property
