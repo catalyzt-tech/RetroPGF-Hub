a1ded00183c0036752ef4f1c743b3785ff12287e
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-09-01 01:23:18 +0700

    feat: responsive navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,17 +1,59 @@
-<script lang="ts">
-  let isMobile = true
+<script>
+  let showMenu = false;
+
+  function toggleNavbar() {
+    showMenu = !showMenu;
+  }
 </script>
 
-<div
-  class="h-[80px] w-full flex flex-row justify-between items-center px-4 lg:px-10"
->
-  <div class="lg:h-fit h-fit w-[200px]">
-    <a href="/"><img src="/img/hub-logo.png" alt="logo" height={100} /></a>
-  </div>
-  <div class="flex flex-row gap-10">
-    <div class="font-semibold"><a href="/archive">History</a></div>
-    <div class="font-semibold">Dashboard</div>
-    <div class="font-semibold">Login</div>
-    <div class="font-semibold"><a href="/signup-form">Nominate Form</a></div>
+<div class="text-base">
+  <div>
+    <nav
+      class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center"
+    >
+      <div class="flex items-center justify-between">
+        <a
+          href="/"
+        >
+          <img src="/img/hub-logo.png" alt="logo" height={100} class="w-40"/>
+        </a>
+        <!-- Mobile menu button -->
+        <div class="flex md:hidden">
+          <button
+            on:click={toggleNavbar}
+            type="button"
+            class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div
+        class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 {showMenu
+          ? 'flex'
+          : 'hidden'}"
+      >
+        <a class="text-gray-800 hover:text-red-400" href="/projects">Projects</a>
+        <a class="text-gray-800 hover:text-red-400" href="/archive">Archive</a>
+        <a class="text-gray-800 hover:text-red-400" href="/nominate"
+          >Nominate</a
+        >
+        <a class="text-gray-800 hover:text-red-400" href="/login">Login</a>
+      </div>
+    </nav>
   </div>
 </div>
