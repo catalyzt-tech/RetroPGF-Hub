5a8f5d1a7ed1166cf10a5fe65094b2b62344dd30
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-09-01 02:30:32 +0700

    feat: homepage update

--- src/components/Home/DialogCard.svelte
+++ src/components/Home/DialogCard.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    let title
+    export let title: string;
+    export let desc: string;
     let button
 </script>
 
-<div class="rounded-[40px] border-black border bg-white px-10 py-8 flex flex-col justify-center items-center">
-    <div>Mentors</div>
-    <div>Hello</div>
-    <div>button</div>
+<div class="rounded-[30px] border-black border bg-white px-10 py-4 flex flex-col justify-center items-center max-w-xs">
+    <div class="font-bold text-lg mb-3">{title}</div>
+    <div class="text-center mb-3">{desc}</div>
+    <div class="">button</div>
 
 
 </div>
