2ddffd449010b06a145c3125506119fe58bff33e
Billy19191 <bill.ratchanon@gmail.com>
2023-09-01 23:46:44 +0700

    feat: relative import, loading handler, and fill detail to card

--- src/routes/archive/+page.svelte
+++ src/routes/archive/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import Card from '../../components/Archive/Card.svelte'
-  // import Data from '../../data/results.json'
   import Navbar from '../../components/Navbar.svelte'
   import Footer from '../../components/Footer.svelte'
+  import { load } from './[slug]/+page'
 
   interface ProjectData {
     'Project Name': string
@@ -13,22 +13,20 @@
     'Project Profile': string
   }
 
-  let Data: ProjectData = []
+  let Data: ProjectData[] = []
+  let loading = true
   onMount(async () => {
     try {
       const response = await fetch('src/data/results.json')
-
-      if (!response.ok) {
-        throw new Error(`Fetch failed with status ${response.status}`)
-      }
-
       Data = await response.json()
-      console.log(Data)
+      // console.log(Data)
     } catch (error) {
       console.error('Error importing data:', error)
     }
   })
-
+  onMount(async () => {
+    loading = (await !Data) ? true : false
+  })
   const maxWidthCard: any = () => {
     const allCard = document.querySelector('.allcard')
     const filter: any = document.querySelector('.filter')
@@ -42,45 +40,59 @@
   }
 </script>
 
-<Navbar />
-<div class="h-[10em] overflow-hidden">
-  <img src="./img/opsunsmile.png" alt="OPBanner" class="w-full" />
-</div>
-<div class="px-[10em]">
-  <h1 class="flex justify-center font-bold mt-6 text-[40px]">
-    RetroPGF 2 Nominated Projects
-  </h1>
-  <div class="flex justify-center my-3">
-    <div class="text-[20px] max-w-[50em] text-base text-center">
-      RetroPGF Round 2 stretched over multiple months and involved participation
-      from hundreds of projects and community members across the Optimism
-      Collective.
+<div class="flex flex-col min-h-screen">
+  <Navbar />
+  {#if loading}
+    <div class="flex flex-row justify-center font-bold text-3xl my-64">
+      Loading
     </div>
-  </div>
-  <div class="flex justify-center text-base font-semibold my-2">
-    Read More &gt
-  </div>
-  <div class="flex justify-end">
-    <select
-      on:change={filterRound}
-      class="filter my-2 p-3 border-2 border-black rounded-lg"
-    >
-      <option>Round 1</option>
-      <option selected>Round 2</option>
-    </select>
-  </div>
-  {#if round == 'Round 2'}
-    <div class="allcard flex flex-wrap justify-center">
-      {#each Data as project}
-        <Card name={project['Project Name']} category={project['Category']} />
-      {/each}
+  {:else if !loading}
+    <div class="h-[10em] overflow-hidden">
+      <img src="./img/opsunsmile.png" alt="OPBanner" class="w-full" />
     </div>
-  {:else if round == 'Round 1'}
-    <div class="allcard flex flex-wrap justify-center">
-      {#each Data as project}
-        <Card name={project['Project Name']} category={project['Category']} />
-      {/each}
+    <div class="px-[10em]">
+      <h1 class="flex justify-center font-bold mt-6 text-[40px]">
+        RetroPGF 2 Nominated Projects
+      </h1>
+      <div class="flex justify-center my-3">
+        <div class="text-[20px] max-w-[50em] text-base text-center">
+          RetroPGF Round 2 stretched over multiple months and involved
+          participation from hundreds of projects and community members across
+          the Optimism Collective.
+        </div>
+      </div>
+      <div class="flex justify-center text-base font-semibold my-2">
+        Read More &gt
+      </div>
+      <div class="flex justify-end">
+        <select
+          on:change={filterRound}
+          class="filter my-2 p-3 border-2 border-black rounded-lg"
+        >
+          <option>Round 1</option>
+          <option selected>Round 2</option>
+        </select>
+      </div>
+      {#if round == 'Round 2'}
+        <div class="allcard flex flex-wrap justify-center">
+          {#each Data as project}
+            <Card
+              name={project['Project Name']}
+              category={project['Category']}
+            />
+          {/each}
+        </div>
+      {:else if round == 'Round 1'}
+        <div class="allcard flex flex-wrap justify-center">
+          {#each Data as project}
+            <Card
+              name={project['Project Name']}
+              category={project['Category']}
+            />
+          {/each}
+        </div>
+      {/if}
     </div>
   {/if}
+  <div class="flex-grow"><Footer /></div>
 </div>
-<Footer />
--- src/routes/archive/[slug]/+page.js
+++ src/routes/archive/[slug]/+page.js
@@ -1,17 +1,11 @@
-import fs from 'fs'
-export const load = ({ params }) => {
+export const load = async ({ params }) => {
   try {
-    // console.log(params + 'and' + params.slug)
-    const data = fs.readFileSync(
-      `public/data/retroPGF2-dataset/each_project/${params.slug}/info.json`
-    )
+    const path = `../../../../public/data/retroPGF2-dataset/each_project/${params.slug}/info.json`
+    const response = await fetch(path)
+    const data = await response.json()
     console.log(data)
-    const fetchProjectTemp = data.toString()
-    const fetchProject = JSON.parse(fetchProjectTemp)
-    console.log(fetchProject)
-    return { detail: fetchProject }
+    return { detail: data }
   } catch (err) {
     console.log(err)
   }
 }
-//param = object, .slug is property
