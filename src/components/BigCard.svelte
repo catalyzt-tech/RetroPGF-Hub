03d5b0033fa65514c892d59b862c795d293fba55
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-09-03 00:45:33 +0700

    feat: responsive /projects

--- src/components/BigCard.svelte
+++ src/components/BigCard.svelte
@@ -1,27 +1,30 @@
 <script>
 </script>
 
-<div>
-  <div
-    class="relative border-2 border-black rounded-3xl w-[40em] h-72 p-6 my-6 mx-5 overflow-hidden"
-  >
-    <div class="absolute w-52 rounded-lg overflow-hidden top-8">
-      <img src="img/test_logo.png" alt="Logo" />
-    </div>
-    <div class="absolute left-64 text-[20px] font-bold">TokenUnlock</div>
-    <div class="absolute left-64 top-[4.5rem] line-clamp-4 w-[25em] text-sm">
+<div
+  class="border border-black rounded-3xl p-4 flex justify-center items-center w-96 lg:w-[420px]"
+>
+  <div class="h-full">
+    <img src="img/test_logo.png" class="rounded-lg overflow-hidden object-contain" width={1500} height={1500} alt="Logo" />
+  </div>
+  
+  <div class="flex flex-col ml-4">
+    <div class="text-xl font-bold">TokenUnlock</div>
+    <div class="line-clamp-3 lg:line-clamp-4 text-sm my-2">
       Token unlocks refer to the release of previously locked or restricted
       tokens into the market. These tokens become available for trading, buying,
       and selling after the end of their vesting period. The vesting period is
       the duration during which the tokens are restricted.
     </div>
-    <div
-      class="absolute bottom-11 left-64 bg-gradient-to-r from-[#000000] to-[#DA0000] rounded-2xl px-6 py-3 w-[14em] text-white font-bold text-sm"
-    >
-      End User Experience & Adoption
-    </div>
-    <div class="absolute bottom-16 right-20 font-medium text-sm">
-      View More &gt
+    <div class="flex items-center">
+      <div
+        class="bg-gradient-to-r from-[#000000] to-[#DA0000] rounded-2xl px-3 py-2 text-white font-bold text-xs mr-2"
+      >
+        EUE      
+        <br class="hidden lg:block">
+        & Adoption
+      </div>
+      <div class="font-medium text-xs">View More &gt</div>
     </div>
   </div>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,19 @@
 <script>
-  import Navbar from '../../components/Navbar.svelte'
-  import Footer from '../../components/Footer.svelte'
-  import BigCard from '../../components/BigCard.svelte'
+  import Navbar from "../../components/Navbar.svelte";
+  import Footer from "../../components/Footer.svelte";
+  import BigCard from "../../components/BigCard.svelte";
 </script>
 
 <Navbar />
 <div>
-  <h1 class="text-[40px] flex justify-center font-bold">
-    Discovery Project nominated by the community
-  </h1>
-  <p class="text-md flex justify-center">Read more about RetroPGF3 here</p>
-  <div class="flex flex-wrap justify-center">
+  <img src="./img/bg.png" class="w-full" alt="" />
+  <div class="my-8">
+    <h1 class="text-[40px] flex justify-center font-bold text-center">
+      Discovery Project nominated by the community
+    </h1>
+    <p class="text-md flex justify-center text-gray-600 mt-2">Read more about RetroPGF 3 here</p>
+  </div>
+  <div class="flex flex-wrap justify-center gap-x-4 gap-y-6">
     <BigCard />
     <BigCard />
     <BigCard />
