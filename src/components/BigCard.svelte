d53d8b9cf4f00d274bd5e7ed437fdf212254a143
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-09-03 02:59:15 +0700

    feat: responsive Bigcard

--- src/components/BigCard.svelte
+++ src/components/BigCard.svelte
@@ -2,29 +2,38 @@
 </script>
 
 <div
-  class="border border-black rounded-3xl p-4 flex justify-center items-center w-96 lg:w-[420px]"
+  class="border border-black rounded-3xl p-6 lg:p-4 flex justify-center items-center flex-wrap lg:flex-nowrap w-80 lg:w-[560px]"
 >
-  <div class="h-full">
+  <div class="lg:h-full">
     <img src="img/test_logo.png" class="rounded-lg overflow-hidden object-contain" width={1500} height={1500} alt="Logo" />
   </div>
-  
-  <div class="flex flex-col ml-4">
-    <div class="text-xl font-bold">TokenUnlock</div>
-    <div class="line-clamp-3 lg:line-clamp-4 text-sm my-2">
-      Token unlocks refer to the release of previously locked or restricted
-      tokens into the market. These tokens become available for trading, buying,
-      and selling after the end of their vesting period. The vesting period is
-      the duration during which the tokens are restricted.
-    </div>
-    <div class="flex items-center">
-      <div
-        class="bg-gradient-to-r from-[#000000] to-[#DA0000] rounded-2xl px-3 py-2 text-white font-bold text-xs mr-2"
-      >
-        EUE      
-        <br class="hidden lg:block">
-        & Adoption
+  <div class="flex flex-col justify-between lg:ml-4 lg:h-full">
+    <div class="flex flex-col my-3 lg:my-0">
+      <div class="text-2xl lg:text-xl font-bold">TokenUnlock</div>
+      <div class="my-3">
+        <div class="line-clamp-3 lg:line-clamp-4 text-sm">
+          Token unlocks refer to the release of previously locked or restricted
+          tokens into the market. These tokens become available for trading, buying,
+          and selling after the end of their vesting period. The vesting period is
+          the duration during which the tokens are restricted.
+        </div>
+        <div class="text-xs shrink-0 font-bold">View More &gt</div>
       </div>
-      <div class="font-medium text-xs shrink-0">View More &gt</div>
+      <div class="flex items-center">
+        <div
+          class="bg-gradient-to-r from-[#000000] to-[#DA0000] rounded-2xl px-3 py-2 text-white font-bold text-xs mr-2"
+        >
+          EUE & Adoption
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-x-2 items-center mt-2 lg:mt-0">
+      <img src="./icons/thumb.svg" class="w-6 opacity-80" alt="" />
+      <div>169</div>
+      <img src="./icons/thumb.svg" class="w-6 rotate-180 opacity-80" alt="" />
+      <div>169</div>
+      <img src="./icons/comment.svg" class="w-6 opacity-80" alt="" />
+      <div>2</div>
     </div>
   </div>
 </div>
