fca1744277085757178438974261653f6a510427
Billy19191 <bill.ratchanon@gmail.com>
2023-09-07 00:03:35 +0700

    feat: continue design login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,8 +10,8 @@
   } from 'firebase/auth'
   import { onMount } from 'svelte'
 
-  let email = ''
-  let password = ''
+  let email: any = ''
+  let password: any = ''
 
   onMount(() => {
     // check if user is login
@@ -62,65 +62,80 @@
   }
 </script>
 
-<div class="flex flex-col">
-  <h1 class="text-2xl">Login</h1>
-  <button
-    class="text-red-300 bg-black"
-    on:click={() => handleLogin(googleProvider)}
-  >
-    Google Login
-  </button>
-  <button
-    class="text-red-600 bg-blue-500"
-    on:click={() => handleLogin(githubProvider)}
-  >
-    Github Login
-  </button>
-  <div class="flex items-center justify-center">
-    <div
-      class="px-8 py-6 mt-4 text-left bg-white rounded-xl border-2 border-black shadow-lg"
-    >
-      <div class="flex justify-center">
-        <img src="/img/hub-logo.png" alt="logo" height={100} class="w-40" />
-      </div>
-      <h3 class="text-xl font-bold text-center text-red-600 mt-3">Login</h3>
-      <form action="">
-        <div class="mt-4">
-          <div>
-            <label class="block" for="email"
-              >Email<label>
-                <input
-                  type="text"
-                  placeholder="Email"
-                  class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
-                  bind:value={email}
-                  required
-                />
-              </label></label
-            >
-          </div>
+<div class="flex flex-col items-center justify-center">
+  <div class="flex-grow">
+    <div class="flex items-center justify-center">
+      <div
+        class="px-8 py-6 mt-4 text-left bg-white rounded-xl border-2 border-black shadow-lg"
+      >
+        <h3 class="text-2xl font-bold text-center mt-3">Sign In</h3>
+        <form action="">
           <div class="mt-4">
-            <label class="block"
-              >Password<label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
-                  bind:value={password}
-                  required
-                />
-              </label></label
-            >
-          </div>
-          <div class="flex items-baseline justify-between">
+            <div>
+              <label class="block font-medium" for="email"
+                >Email<label>
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
+                    bind:value={email}
+                    required
+                  />
+                </label></label
+              >
+            </div>
+            <div class="mt-4">
+              <label class="block font-medium"
+                >Password<label>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
+                    bind:value={password}
+                    required
+                  />
+                </label></label
+              >
+            </div>
+            <div class="flex items-baseline justify-between my-1">
+              <button
+                class="px-6 py-2 mt-4 text-white font-semibold bg-red-600 rounded-lg hover:bg-black hover:text-white transition ease-linear duration-200"
+                on:click={handlePasswordLogin}>Login</button
+              >
+              <a href="#" class="text-sm hover:underline">Forgot password?</a>
+            </div>
+            <div class="mt-3 my-1 flex mx-3 justify-center font-semibold">
+              Or
+            </div>
             <button
-              class="px-6 py-2 mt-4 text-white font-bold bg-red-600 rounded-lg hover:bg-gray-200 hover:text-gray-800"
-              on:click={handlePasswordLogin}>Login</button
+              class="px-4 py-3 mt-4 mr-1 text-black font-medium bg-gray-200 rounded-lg hover:bg-black hover:text-white transition ease-linear duration-00"
+              on:click={() => handleLogin(googleProvider)}
             >
-            <a href="#" class="text-sm hover:underline">Forgot password?</a>
+              <img
+                src="img/google_logo.png"
+                alt="google"
+                width={20}
+                height={20}
+                class="inline mr-2 mb-1"
+              />
+              Sign in with Google
+            </button>
+            <button
+              class="px-4 py-3 mt-4 mr-1 text-black font-medium bg-gray-200 rounded-lg hover:invert transition ease-linear duration-200"
+              on:click={() => handleLogin(githubProvider)}
+            >
+              <img
+                src="img/github_logo.png"
+                alt="google"
+                width={20}
+                height={20}
+                class="inline mr-2 mb-1"
+              />
+              Sign in with Github
+            </button>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </div>
