d2d23ff0016ac64c6f5a49901fa40a4486d989e0
Billy19191 <bill.ratchanon@gmail.com>
2023-09-21 21:45:30 +0700

    feat: history page customize

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,18 +1,20 @@
 <script>
+  import Button from '@/components/History/Button.svelte'
+  const round = ['Round 1', 'Round 2', 'Round 3']
 </script>
 
-<div class="relative flex flex-col h-[calc(100vh-10rem)] overflow-hidden">
+<div class="relative flex flex-col overflow-hidden md:h-fit">
   <div class="w-full min-h-[150px] lg:h-[12em] overflow-hidden">
     <img src="./img/history_banner_2.png" alt="OPBanner" class="w-full" />
   </div>
   <div class="flex justify-center text-[40px] font-bold mt-8">History</div>
-  <div class="flex justify-center text-center mx-52">
+  <em class="flex justify-center text-center mx-10 md:mx-52">
     The Optimism team has long been in search of a solution on how to
     sustainably fund public goods, and we now have the structure of our first
     experiment thanks to a brilliant design by Vitalik Buterin.
-  </div>
+  </em>
   <br />
-  <div class="flex justify-center text-center mx-52">
+  <div class="flex justify-center text-center mx-10 md:mx-52">
     The core principle behind the concept of retroactive public goods funding is
     simple: it’s easier to agree on what was useful than what will be useful.
     The former is still often a source of disagreement, but it’s a type of
@@ -21,26 +23,16 @@
     regular voting).
   </div>
 
-  <div class="flex flex-wrap justify-center space-x-40 mt-20">
-    <div
-      class="flex text-xl px-16 py-5 border-2 bg-white font-semibold border-black rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-white transition ease-in-out duration-200"
-    >
-      RetroPGF 1
-    </div>
-    <div
-      class="flex text-xl px-16 py-5 border-2 bg-white font-semibold border-black rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-white transition ease-in-out duration-200"
-    >
-      RetroPGF 2
-    </div>
-    <div
-      class="flex text-xl px-16 py-5 border-2 bg-white font-semibold border-black rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-white transition ease-in-out duration-200"
-    >
-      RetroPGF 3
-    </div>
+  <div class="flex flex-wrap justify-center mt-7">
+    {#each round as r}
+      <Button round={r} />
+    {/each}
   </div>
-  <div
-    class="absolute bottom-[-30rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 justify-center items-center mt-6 my-10"
-  >
-    <img src="./img/op_sun.svg" width={400} />
+  <div class="flex justify-center items-center h-[10em] mt-20 my-10">
+    <img
+      src="./img/op_sun.svg"
+      alt="op sun"
+      class="mt-40 w-[20em] lg:w-[25em] hover:animate-spin 10s"
+    />
   </div>
 </div>
