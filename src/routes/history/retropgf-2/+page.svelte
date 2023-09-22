624f1f1be1916c7de72a88c65ce5650e82379000
Billy19191 <bill.ratchanon@gmail.com>
2023-09-22 23:51:16 +0700

    feat: retropgf2 page

--- src/routes/history/retropgf-2/+page.svelte
+++ src/routes/history/retropgf-2/+page.svelte
@@ -1,1 +1,261 @@
-<div>RetroPGF 2</div>
+<script>
+</script>
+
+<div class="flex flex-col bg-white">
+  <div class="w-screen h-64 overflow-hidden">
+    <img src="/img/retropgf2_banner.jpeg" class="w-full" alt="retropgf1" />
+  </div>
+  <div class="flex flex-col px-10 md:px-20 lg:px-52 overflow-hidden">
+    <div
+      class="flex justify-center text-center font-semibold text-[40px] mt-8 mb-4"
+    >
+      RetroPGF 2
+    </div>
+    <div class="flex justify-center text-center">
+      The Citizens' House employs a unique approach called "retroactive public
+      goods funding" (RetroPGF), which believes it's easier to support what's
+      proven valuable rather than guessing what might be useful. Each RetroPGF
+      round begins with a defined funding scope, initially set by the Optimism
+      Foundation and later by the community. Projects are then evaluated by the
+      Citizens' House based on their impact within this scope, determining
+      funding allocations. Optimism is committed to dedicating 20% of its OP
+      token allocation to RetroPGF, and future rounds will tap into additional
+      funding sources. This iterative process blends theory, practice, and
+      community feedback to shape a retroactive funding system that benefits
+      all.
+    </div>
+    <div class="flex justify-center items-center w-full overflow-hidden my-10">
+      <img
+        src="/img/retropgf2_banner_3.png"
+        class="max-w-[70em] rounded-full"
+        alt="retropgf1 "
+      />
+    </div>
+    <section>
+      <div class="text-2xl font-bold italic">Experiment Overview</div>
+      <div class=" mt-2">
+        The Optimism Collective recently held its second round of Retroactive
+        Public Goods Funding (RetroPGF) in the first quarter of 2023. The
+        RetroPGF 2 allocation of 10 million OP tokens was directed towards
+        funding public goods that promote the development and usage of the OP
+        Stack. The ultimate goal of the Collective is to establish a global
+        system that rewards positive contributions with individual profit. The
+        RetroPGF mechanism serves as an experiment towards achieving this goal
+        of <span class="font-medium">"impact = profit"</span>. By funding public
+        goods in a sustainable manner, the Collective can foster a thriving
+        ecosystem and a stronger economy. Overall, this initiative aims to
+        create a positive impact on the world. The Optimism Collective believes
+        that building public goods should be profitable.
+      </div>
+      <div
+        class="flex justify-center items-center w-full overflow-hidden my-10"
+      >
+        <img src="/img/Bicameral.webp" alt="bicameral" class="max-w-[70em]" />
+      </div>
+    </section>
+    <section>
+      <div class="text-2xl font-bold italic">Process and Timeline</div>
+      <div class="mt-3">
+        This is the kick-off for the project nominations of the recently
+        announced RetroPGF Round 2. Optimism’s vision is to build a global
+        system where positive impact to the Collective is rewarded by profit to
+        the individual. RetroPGF is an experimental mechanism to realize this
+        goal of “impact = profit”. For more information, read the announcement
+        post or visit our governance documentation.
+      </div>
+      <div>
+        <ol
+          class="relative border-l border-[#ff1818] dark:border-[#ff1818] my-6 ml-2"
+        >
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >Prior January 2023
+            </time>
+            <div class="text-md font-medium">Badgeholder selection</div>
+            <p class="text-gray-600">
+              Badgeholders have the power to distribute OP tokens to projects.
+              They’re instrumental in running an effective RetroPGF round. For
+              RetroPGF Round 2, badgeholders were selected across four different
+              criteria.
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >17 - 31st January 2023
+            </time>
+            <div class="text-md font-medium">Nominations</div>
+            <p class="text-gray-600">
+              anyone could nominate a project on the forum (opens new window) by
+              providing a name, impact description and link to Github/Twitter
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >7 - 21st February 2023
+            </time>
+            <div class="text-md font-medium">Project profile creation</div>
+            <p class="text-gray-600">
+              Projects had to create a profile (opens new window) where they
+              were asked for general information, as well as a description of
+              their project and its impact. Information provided by Projects can
+              be viewed on the RetroPGF discovery page (opens new window).
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >7 - 21st March 2023
+            </time>
+            <div class="text-md font-medium">Voting</div>
+            <p class="text-gray-600">
+              Badgeholders were provided with a badgeholder manual (opens new
+              window) and asked to evaluate and vote on nominated projects via a
+              voting form
+            </p>
+          </li>
+          <li class="ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >After November 2022
+            </time>
+            <div class="text-md font-medium">Payout / Distribution</div>
+            <p class="text-gray-600">
+              Projects received rewards based on their received quadratic votes.
+            </p>
+          </li>
+        </ol>
+      </div>
+    </section>
+    <section>
+      <div class="text-2xl font-bold italic">Scope of Round 2</div>
+      <div>
+        RetroPGF Round 2 has a clear mission which is allocate 10 million OP
+        tokens to projects that enhance the development and utilization of the
+        OP Stack. These projects fall into three key categories:
+      </div>
+      <ol class="list-disc ml-5">
+        <li>
+          <div class="font-medium">Infrastructure + Dependencies</div>
+          <p class="text-gray-600">
+            Software used to build or deploy the OP Stack; contributions to
+            protocols or standards upon which the OP Stack runs; experiments
+            that support future development of the core OP Stack protocol.
+          </p>
+        </li>
+        <li>
+          <div class="font-medium">Tooling + Utilities</div>
+          <p class="text-gray-600">
+            Work that helps builders create applications on Optimism mainnet,
+            build on the OP Stack, interact with governance of the Collective,
+            or use applications built on Optimism.
+          </p>
+        </li>
+        <li>
+          <div class="font-medium">Education</div>
+          <p class="text-gray-600">
+            Work to spread awareness and knowledge of how Optimism works,
+            whether technically or socially.
+          </p>
+        </li>
+      </ol>
+    </section>
+    <section>
+      <div class="text-2xl font-bold italic">Nominate Projects</div>
+      <div>
+        To nominate a project for RetroPGF, visit the designated forum posts for
+        your chosen nomination category:
+      </div>
+      <div class=" flex flex-wrap flex-grow justify-between my-4 max-w-full">
+        <div
+          class="flex-grow lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <span class="font-medium text-lg">Infrastructure + Dependencies</span>
+        </div>
+        <div
+          class="flex-grow lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <span class="font-medium text-lg">Tooling + Utilities</span>
+        </div>
+        <div
+          class="flex-grow lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <span class="font-medium text-lg">Education</span>
+        </div>
+      </div>
+      <div class="text-lg font-semibold">
+        What is things to consider when nominating a project?
+      </div>
+      <div class="mt-5">
+        <ol class="list-disc ml-5">
+          <li>
+            <div class="font-medium">Provide Impact Details:</div>
+            <p class="text-gray-600">
+              Describe how the project has made a positive impact within the
+              scope of this round
+            </p>
+          </li>
+          <li>
+            <div class="font-medium">Choose the Right Category:</div>
+            <p class="text-gray-600">
+              Ensure that you nominate projects in the relevant category.
+            </p>
+          </li>
+          <li>
+            <div class="font-medium">Avoid Duplicates:</div>
+            <p class="text-gray-600">
+              Check if the project has already been nominated to prevent
+              duplication.
+            </p>
+          </li>
+        </ol>
+      </div>
+      <div class="text-lg font-semibold my-4">For project owner</div>
+      <ol class="list-disc ml-5">
+        <li>
+          <div class="font-medium">Read the Project Manual</div>
+          <p class="text-gray-600">
+            Familiarize yourself with the Project Manual, which provides an
+            overview of the process and answers frequently asked questions.
+          </p>
+        </li>
+        <li>
+          <div class="font-medium">Opt-In and Create a Project Profile</div>
+          <p class="text-gray-600">
+            To be eligible, opt into the round and complete your project profile
+            in the RetroPGF application manager by Feb 21. This profile should
+            include additional information about your project and verification
+            of your GitHub or Twitter account.
+          </p>
+        </li>
+        <li>
+          <div class="font-medium">Stay Updated</div>
+          <p class="text-gray-600">
+            Keep an eye out for the RetroPGF application manager, set to launch
+            by the end of January. More details will follow soon!
+          </p>
+        </li>
+      </ol>
+    </section>
+  </div>
+</div>
+
+<style>
+</style>
