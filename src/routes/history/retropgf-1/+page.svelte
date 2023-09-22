0ce32c7c20976c7ad4179c1228edd691fb6d04fc
Billy19191 <bill.ratchanon@gmail.com>
2023-09-22 14:42:57 +0700

    feat: detail retropgf1 + page

--- src/routes/history/retropgf-1/+page.svelte
+++ src/routes/history/retropgf-1/+page.svelte
@@ -1,1 +1,78 @@
-<div>RetroPGF 1</div>
+<script>
+</script>
+
+<div class="flex flex-col">
+  <div class="w-screen h-64 overflow-hidden">
+    <img src="/img/retropgf1_banner_2.jpg" class="w-full" alt="retropgf1" />
+  </div>
+  <div class="flex flex-col px-52 overflow-hidden">
+    <div
+      class="flex justify-center text-center font-semibold text-[40px] mt-8 mb-4"
+    >
+      RetroPGF 1
+    </div>
+    <div class="flex justify-center text-center">
+      Welcome to RetroPGF Experiment #1, where we're pioneering a groundbreaking
+      approach to funding public goods! We're excited to announce our commitment
+      to donating $1 million of network profits in this inaugural experiment. At
+      RetroPGF, we're all about leveling the playing field between profit-driven
+      startups and nonprofit/open-source projects. Our goal is to provide the
+      same benefits and incentives for those who contribute to the greater good
+      of technology.
+    </div>
+    <div class="flex justify-center items-center w-full overflow-hidden my-10">
+      <img
+        src="/img/summer_1.png"
+        class="max-w-[70em] rounded-full"
+        alt="retropgf1"
+      />
+    </div>
+    <div class="text-xl font-bold italic">Experiment Overview</div>
+    <div class="flex justify-center mt-3 mb-10">
+      Our first experiment is focused on creating a fair and effective system
+      for allocating funds to deserving projects. We understand that this
+      process is challenging, and we're dedicated to refining it through
+      multiple iterations. In this MVP (Minimum Viable Product) phase, a group
+      of 24 "badge holders" will use quadratic voting to decide how to allocate
+      the $1 million. These badge holders consist of 8 Optimists and 16 members
+      from the Ethereum community. The allocation process will primarily
+      consider the value projects have contributed to the Optimistic Ethereum
+      ecosystem.
+    </div>
+    <div class="text-xl font-bold italic">Result</div>
+    <div class="flex flex-col mt-3 mb-10">
+      Curious about the outcomes of our first round? Here's a quick summary:
+      <ul class=" flex flex-wrap justify-between ml-5 mt-8">
+        <li
+          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
+        >
+          <span class="font-medium text-lg">Nominated Projects</span>
+          <div class="text-3xl font-medium mt-2">76</div>
+        </li>
+        <li
+          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
+        >
+          <span class="font-medium text-lg">Projects Awarded Funding</span>
+          <div class="text-3xl font-medium mt-2">58</div>
+        </li>
+        <li
+          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
+        >
+          <span class="font-medium text-lg">Median Funding</span>
+          <div class="text-3xl font-medium mt-2">$14,670</div>
+        </li>
+        <li
+          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
+        >
+          <span class="font-medium text-lg"
+            >Top 10% of Projects received over</span
+          >
+          <div class="text-3xl font-medium mt-2">$36,919</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>
