f6f8504dfb1c497f03518934d8a37cccbe19cf65
Billy19191 <bill.ratchanon@gmail.com>
2023-09-22 16:08:11 +0700

    feat: timeline and process rpgf1

--- src/routes/history/retropgf-1/+page.svelte
+++ src/routes/history/retropgf-1/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col bg-white">
   <div class="w-screen h-64 overflow-hidden">
     <img src="/img/retropgf1_banner_2.jpg" class="w-full" alt="retropgf1" />
   </div>
@@ -27,49 +27,147 @@
         alt="retropgf1"
       />
     </div>
-    <div class="text-xl font-bold italic">Experiment Overview</div>
-    <div class="flex justify-center mt-3 mb-10">
-      Our first experiment is focused on creating a fair and effective system
-      for allocating funds to deserving projects. We understand that this
-      process is challenging, and we're dedicated to refining it through
-      multiple iterations. In this MVP (Minimum Viable Product) phase, a group
-      of 24 "badge holders" will use quadratic voting to decide how to allocate
-      the $1 million. These badge holders consist of 8 Optimists and 16 members
-      from the Ethereum community. The allocation process will primarily
-      consider the value projects have contributed to the Optimistic Ethereum
-      ecosystem.
-    </div>
-    <div class="text-xl font-bold italic">Result</div>
-    <div class="flex flex-col mt-3 mb-10">
-      Curious about the outcomes of our first round? Here's a quick summary:
-      <ul class=" flex flex-wrap justify-between ml-5 mt-8">
-        <li
-          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
+    <section class="mt-3 mb-5">
+      <div class="text-2xl font-bold italic">Experiment Overview</div>
+      <div class="flex justify-center mt-3">
+        Our first experiment is focused on creating a fair and effective system
+        for allocating funds to deserving projects. We understand that this
+        process is challenging, and we're dedicated to refining it through
+        multiple iterations. In this MVP (Minimum Viable Product) phase, a group
+        of 24 "badgeholders" will use quadratic voting to decide how to allocate
+        the $1 million. These badge holders consist of 8 Optimists and 16
+        members from the Ethereum community. The allocation process will
+        primarily consider the value projects have contributed to the Optimistic
+        Ethereum ecosystem.
+      </div>
+    </section>
+    <section>
+      <div class="text-2xl font-bold italic">Process and Timeline</div>
+      <div class="mt-3">
+        Over the next month, we invite the Optimistic Ethereum community to
+        nominate projects that they believe should receive a portion of this
+        funding. The 24 badgeholders will openly discuss these projects via a
+        public, read-only Discord channel named <span class="italic"
+          >#retroactive-public-goods</span
+        >. Your participation is essential in shaping the future of this
+        initiative.
+      </div>
+      <div>
+        <ol
+          class="relative border-l border-[#ff1818] dark:border-[#ff1818] my-6"
         >
-          <span class="font-medium text-lg">Nominated Projects</span>
-          <div class="text-3xl font-medium mt-2">76</div>
-        </li>
-        <li
-          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
-        >
-          <span class="font-medium text-lg">Projects Awarded Funding</span>
-          <div class="text-3xl font-medium mt-2">58</div>
-        </li>
-        <li
-          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
-        >
-          <span class="font-medium text-lg">Median Funding</span>
-          <div class="text-3xl font-medium mt-2">$14,670</div>
-        </li>
-        <li
-          class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-200"
-        >
-          <span class="font-medium text-lg"
-            >Top 10% of Projects received over</span
+          <li class="mb-6 ml-4">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >Prior October 2022
+            </time>
+            <div class="text-md font-medium">Badgeholder selection</div>
+            <p class="text-gray-600">
+              In RetroPGF Round 1, 24 badgeholders, made up of 8 Optimists and
+              16 Ethereum community members, were selected to vote on
+              distributing retrofunding to nominated projects.
+            </p>
+          </li>
+          <li class="mb-6 ml-4">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >5th - 21st October 2022
+            </time>
+            <div class="text-md font-medium">Nominations</div>
+            <p class="text-gray-600">
+              Anyone could nominate a project via a form submission (opens new
+              window) by providing a project name, project lead name, project
+              lead e-mail and impact description.
+            </p>
+          </li>
+          <li class="mb-6 ml-4">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >24th October 2022 - 1th November 2022
+            </time>
+            <div class="text-md font-medium">Voting</div>
+            <p class="text-gray-600">
+              Badgeholders were provided with a badgeholder manual (opens new
+              window) and asked to evaluate and vote on nominated projects via
+              quadraticvote.co's interface (opens new window).
+            </p>
+          </li>
+          <li class="ml-4">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >After November 2022
+            </time>
+            <div class="text-md font-medium">Payout / Distribution</div>
+            <p class="text-gray-600">
+              Projects received rewards based on their received quadratic votes.
+            </p>
+          </li>
+        </ol>
+      </div>
+    </section>
+    <section class="mb-5">
+      <div class="text-2xl font-bold italic">Result</div>
+      <div class="flex flex-col mt-3 mb-10">
+        Curious about the outcomes of our first round? Here's a quick summary:
+        <ul class=" flex flex-wrap justify-between ml-5 mt-8">
+          <li
+            class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
           >
-          <div class="text-3xl font-medium mt-2">$36,919</div>
-        </li>
-      </ul>
+            <span class="font-medium text-lg">Nominated Projects</span>
+            <div class="text-3xl font-medium mt-2">76</div>
+          </li>
+          <li
+            class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+          >
+            <span class="font-medium text-lg">Projects Awarded Funding</span>
+            <div class="text-3xl font-medium mt-2">58</div>
+          </li>
+          <li
+            class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+          >
+            <span class="font-medium text-lg">Median Funding</span>
+            <div class="text-3xl font-medium mt-2">$14,670</div>
+          </li>
+          <li
+            class="border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+          >
+            <span class="font-medium text-lg"
+              >Top 10% of Projects received over</span
+            >
+            <div class="text-3xl font-medium mt-2">$36,919</div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        For a detailed list of funded projects and their allocations, please
+        check it out on <a
+          href="/archive"
+          class="text-[#ff1818] hover:underline">archive page</a
+        >. For Vitalik's review of the round,
+        <a
+          href="https://vitalik.ca/general/2021/11/16/retro1.html"
+          class="text-[#ff1818] hover:underline">click here.</a
+        >
+      </div>
+    </section>
+    <section>
+      <div class="text-2xl font-bold italic">Reflection and Learning</div>
+    </section>
+    <div class="text-lg font-medium my-6">
+      Thank you for joining us in this exciting experiment to make the internet
+      a better place for all!
     </div>
   </div>
 </div>
