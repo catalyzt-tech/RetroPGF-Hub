918455f52b02b8ed747d9da271a8d630b136eef3
Billy19191 <bill.ratchanon@gmail.com>
2023-09-26 20:22:55 +0700

    feat: draft retropgf 3 page

--- src/routes/history/retropgf-3/+page.svelte
+++ src/routes/history/retropgf-3/+page.svelte
@@ -1,1 +1,171 @@
-<div>RetroPGF 3</div>
+<div class="flex flex-col bg-white">
+  <div class="w-screen h-64 overflow-hidden">
+    <img src="/img/retropgf3_banner.jpg" class="w-full" alt="retropgf3" />
+  </div>
+  <div class="flex flex-col px-10 md:px-20 lg:px-52 overflow-hidden">
+    <div
+      class="flex justify-center text-center font-semibold text-[40px] mt-8 mb-4"
+    >
+      RetroPGF 3
+    </div>
+    <div class="flex justify-center text-center">
+      RetroPGF Rounds 1 & 2 were a resounding success, benefitting numerous
+      dedicated individuals and teams who are passionate about creating public
+      goods. These retroactive grants have not only made a significant impact on
+      their projects but have also reinforced their commitment to the Optimism
+      ecosystem. At the heart of the Optimism Collective's mission is the belief
+      that those who contribute to public goods should be rewarded for their
+      efforts. RetroPGF is like a refreshing twist on the traditional market
+      economy, specifically designed to champion public goods. And the best
+      part? It's already proving to be a game-changer for many builders within
+      the Collective. RetroPGF, in essence, serves as Optimism's vehicle for
+      nurturing the development of a free, open, and decentralized internet that
+      puts the power and value back into the hands of those who create and
+      maintain it.
+    </div>
+    <div class="flex justify-center items-center w-full overflow-hidden my-10">
+      <img
+        src="/img/retropgf3_banner_2.webp"
+        class="w-[50em] rounded-full"
+        alt="retropgf1 "
+      />
+    </div>
+    <section class="mb-3">
+      <div class="text-3xl font-bold italic">Experiment Overview</div>
+      <div class="mt-3">
+        The Optimism Collective is thrilled to unveil the much-anticipated third
+        round of RetroPGF this autumn! We're spreading the love with a whopping
+        30 million OP set to be distributed among the amazing builders,
+        creators, and educators who've have supported the development and
+        adoption of Optimism.
+      </div>
+      <div class="mt-3">
+        At Optimism, we're on a mission to craft an economy that celebrates and
+        rewards our community members for their incredible contributions. These
+        regular RetroPGF rounds play a pivotal role in fueling Optimism's
+        economy, with the ultimate goal of taking the mantra "impact = profit"
+        and sharing it with the world. Join us in making a difference!
+      </div>
+    </section>
+    <section class="mb-3">
+      <div class="text-3xl font-bold italic">Process and Timeline</div>
+      <div class="mt-3">
+        The Optimism Collective's third round of Retroactive Public Goods
+        Funding (RetroPGF) is set to kick off in the fall of 2023. Stay tuned
+        for your chance to get involved and make a difference!
+      </div>
+      <div>
+        <ol
+          class="relative border-l border-[#ff1818] dark:border-[#ff1818] my-6 ml-2"
+        >
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >1st August - 15th September 2023
+            </time>
+            <div class="text-md font-medium">Badgeholder Selection</div>
+            <p class="text-gray-600">
+              Badgeholders will be selected based on their contributions by
+              three ways
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >19th September - 23rd October 2023
+            </time>
+            <div class="text-md font-medium">Project Applications</div>
+            <p class="text-gray-600">
+              During this phase, builders, creators, and educators had the
+              chance to submit their applications, showcasing their positive
+              impact within the Optimism Collective.Theres no nominations
+              process in this round, instead projects sign-up directly.
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >6th November - 7th December 2023
+            </time>
+            <div class="text-md font-medium">Voting</div>
+            <p class="text-gray-600">
+              Lists application allow badgeholders to leverage the expertise of
+              other badgeholders when they cast their own votes
+            </p>
+          </li>
+          <li class="mb-6 ml-6">
+            <div
+              class="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white"
+            />
+            <time
+              class="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500"
+              >Start early January 2024
+            </time>
+            <div class="text-md font-medium">Results & Token Disbursement</div>
+            <p class="text-gray-600">
+              Projects received rewards based on their received OP allocation
+              votes from each badgeholder, celebrating their valuable work.
+            </p>
+          </li>
+        </ol>
+      </div>
+    </section>
+    <section class="mb-3">
+      <div class="text-3xl font-bold italic">Round Design</div>
+      <div class="mt-3">
+        Retroactive Public Goods Funding is a dynamic experiment, always
+        evolving to better serve our amazing community. In this post, we're
+        taking you behind the scenes to share the nitty-gritty of RetroPGF 3's
+        design process.
+      </div>
+      <div class="mt-3">
+        Let's talk about what we've cooked up for Round 3. These design choices
+        are like ingredients in our recipe for success, and they come with their
+        own set of assumptions. We're super excited to test these out in the
+        upcoming Round and use your feedback to keep making RetroPGF even
+        better.
+      </div>
+      <div
+        class=" flex flex-wrap flex-grow justify-between max-w-full mt-2 mb-5"
+      >
+        <div
+          class="flex-grow mt-4 lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <div class="text-4xl">📬</div>
+          <span class="font-medium text-lg">RetroPGF Sign up</span>
+          <div class="mt-2">Gathering the right data from Projects</div>
+        </div>
+        <div
+          class="flex-grow mt-4 lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <div class="text-4xl">📬</div>
+          <span class="font-medium text-lg">Lists</span>
+          <div class="mt-2">Scaling the Evaluation of Projects</div>
+        </div>
+        <div
+          class="flex-grow mt-4 lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <div class="text-4xl">📬</div>
+          <span class="font-medium text-lg">Impact Evaluation</span>
+          <div class="mt-2">Frameworks & Definitions</div>
+        </div>
+        <div
+          class="flex-grow mt-4 lg:mr-8 border-2 border-black px-6 py-4 rounded-xl hover:bg-[#ff1818] hover:text-white hover:border-[#ff1818] transition ease-in-out duration-300"
+        >
+          <div class="text-4xl">📬</div>
+          <span class="font-medium text-lg">Voting Application</span>
+          <div class="mt-2">How badgeholders express themselves</div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
