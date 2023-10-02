4d76801ad1a42f356f30f7c32993a234d9e2177a
Billy19191 <bill.ratchanon@gmail.com>
2023-10-03 00:39:07 +0700

    feat: change nominated to suggest and set favicon

--- src/components/Suggest-Form/Form.svelte
+++ src/components/Suggest-Form/Form.svelte
@@ -57,28 +57,28 @@
     class="border-2 border-black rounded-2xl mx-36 my-10 p-8 bg-white"
     on:submit|preventDefault={onSubmit}
   >
-    <label class="font-bold text-md"
-      >Project Name
+    <label 
+      >
+      <span class="font-bold text-md">Project Name</span><span class="text-[#FF0000]">*</span>
       <div>
         <input
           type="text"
           name="name"
           bind:value={data.name}
-          placeholder="Name"
+          placeholder="Example Project"
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full"
           required
         />
       </div>
     </label>
     <label class="font-bold text-md"
-      >Project Logo (Icon)
+      >Project Logo / Icon
       <div>
         <input
           type="file"
           on:change={(e) => onFileSelected(e, 'logoFile')}
           accept="image/png,image/jpeg"
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm"
-          required
         />
       </div></label
     >
@@ -90,24 +90,25 @@
           on:change={(e) => onFileSelected(e, 'bannerFile')}
           accept="image/png,image/jpeg"
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm"
-          required
         />
       </div></label
     >
-    <label class="font-bold text-md"
-      >Project Website / Link
+    <label 
+      ><span class="font-bold text-md">Project Website / Link</span><span class="text-[#FF0000]">*</span>
       <div>
         <input
           type="url"
           bind:value={data.website_url}
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full"
+          placeholder = "https://example.project.com"
           required
         />
       </div></label
     >
-    <label class="font-bold text-md"
-      >Which category within the cryptocurrency space describe this project
-      best?
+    <label
+      >
+      <span class="font-bold text-md">Which category within the cryptocurrency space describe this project best?</span><span class="text-[#FF0000]">*</span>
+      
       <div>
         <select
           name="Category"
@@ -122,31 +123,34 @@
         </select>
       </div></label
     >
-    <label class="font-bold text-md"
-      >Project Description
+    <label 
+      >
+      <span class="font-bold text-md">Project Description</span><span class="text-[#FF0000]">*</span>
       <div>
         <textarea
           on:input={dynamicHeight}
           bind:value={data.description}
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full resize-none max-h-32"
+          placeholder="Please describe your project in few sentences or passage."
           required
         />
       </div></label
     >
-    <label class="font-bold text-md"
-      >How has this project contributed to the development and adoption of
-      Optimism?
+    <label 
+      ><span class="font-bold text-md">How has this project contributed to the development and adoption of
+      Optimism?</span><span class="text-[#FF0000]">*</span>
       <div>
         <textarea
           on:input={dynamicHeight}
           bind:value={data.reason}
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full resize-none max-h-32"
+          placeholder ="Please describe how this project has contributed to the development and adoption of Optimism."
           required
         />
       </div></label
     >
-    <label class="font-bold text-md"
-      >Which category describes this project best?
+    <label 
+      ><span class="font-bold text-md">Which category describes this project best?</span><span class="text-[#FF0000]">*</span>
       <div>
         <select
           class="bg-[#EDEDED] px-5 my-5 rounded-lg text-sm w-48 h-12"
@@ -163,23 +167,24 @@
         </select>
       </div></label
     >
-    <label class="font-bold text-md"
-      >Why do you think this project be suited for this category?
+    <label 
+      ><span class="font-bold text-md">Why do you think this project be suited for this category?</span>
       <div>
         <input
           type="text"
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full"
-          required
+          placeholder="Please describe why you think this project be suited for this category."
         />
       </div></label
     >
-    <label class="font-bold text-md"
-      >Contact method of project or project lead
+    <label 
+      ><span class="font-bold text-md">Contact method of project or project lead</span><span class="text-[#FF0000]">*</span>
       <div>
         <input
           type="text"
           bind:value={data.contact}
           class="bg-[#EDEDED] p-4 my-5 rounded-md text-sm w-full"
+          placeholder = "Please provide a contact method of project or project lead."
           required
         />
       </div></label
@@ -187,8 +192,8 @@
     <button
       type="submit"
       name="submit"
-      class="flex justify-center my-3 mx-auto border-[#FF0000] bg-[#FF0000] border-2 px-8 py-2 rounded-xl font-bold text-white"
-      >Submit!</button
+      class="flex justify-center my-3 mx-auto border-[#FF0000] bg-[#FF0000] border-2 px-8 py-2 rounded-xl font-semibold text-white"
+      >Submit</button
     >
   </form>
 </div>
--- src/routes/suggest-form/+page.svelte
+++ src/routes/suggest-form/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import Alertbar from '@/components/Alertbar.svelte'
-  import Form from '@/components/Nominate-Form/Form.svelte'
+  import Form from '@/components/Suggest-Form/Form.svelte'
 </script>
 
 <Alertbar />
@@ -8,7 +8,7 @@
   <div
     class="flex flex-row flex-wrap justify-center text-[40px] font-bold mb-2"
   >
-    Nominate Project
+    Suggest Project
   </div>
   <div class="flex justify-center px-10">
     <div class="max-w-[50em] text-center">
@@ -16,7 +16,7 @@
         class="font-semibold bg-[#FF0000] mx-1 px-4 py-1 rounded-xl text-white"
         >impact = profit</span
       >, we want to ensure that all the relevant projects in the Optimism
-      Collective sign-up for RetroPGF 3. You can nominate a project here so they
+      Collective sign-up for RetroPGF 3. You can suggest a project here so they
       are notified to sign up for RetroPGF 3
     </div>
   </div>
