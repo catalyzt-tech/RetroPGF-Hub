0a46006382bc053e2894b18ac415c30e1ef0692f
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-10-03 01:25:07 +0700

    feat: dynamic api url selection

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const Axios = axios.create({
-    baseURL: import.meta.env.VITE_Backend_Url || "http://localhost:8080",
+    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_Backend_URL_DEV : import.meta.env.VITE_Backend_URL_PROD,
     withCredentials: true
 });
