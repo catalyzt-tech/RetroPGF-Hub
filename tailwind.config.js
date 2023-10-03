c9179614ec4281fe582cd0f5febfdd1823601c8f
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-10-04 01:57:14 +0700

    chore: add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
-      rubik: ['Rubik', 'sans-serif'],
+      colors: {
+        retro: "#FF0000",
+      },
+      rubik: ["Rubik", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
