65fb0ad93b0b06c311c951367ea3da3dee984534
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2023-10-07 23:43:36 +0700

    feat: comment section ui

--- src/components/Projects/DetailCard.svelte
+++ src/components/Projects/DetailCard.svelte
@@ -70,5 +70,35 @@
       </div>
       <div class="mt-1 mb-5 text-[#616161]">{detail?.reason}</div>
     </div>
+
+    <!-- component -->
+    <div class="antialiased mx-10 mb-6 w-fit">
+      <h3 class="mb-4 text-lg font-bold">Comments</h3>
+
+      <div class="space-y-4">
+        <div class="flex">
+          <div class="flex-shrink-0 mr-3">
+            <img
+              class="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
+              src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
+              alt=""
+            />
+          </div>
+          <div
+            class="flex-1 border rounded-lg w-full px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
+          >
+            <strong>Sarah</strong>
+            <span class="text-xs text-gray-400">3:34 PM</span>
+            <p class="text-sm">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
   </div>
 </div>
