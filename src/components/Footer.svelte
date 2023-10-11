8ce1c2642d0f453a6a7127b6f30be92dc0d4e300
Billy19191 <bill.ratchanon@gmail.com>
2023-10-11 22:22:17 +0700

    feat: responsive footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
-<footer class=" relative bg-[#FFE8E8] pt-5 pb-2 text-sm">
-  <div class="absolute top-7 left-10">
+<footer class="md:relative bg-[#FFE8E8] pt-5 pb-2 text-sm">
+  <div class="flex my-4 ml-10 md:absolute md:top-7 md:left-10 md:my-0 md:ml-0">
     <div class="flex space-x-3">
       <a href="https://www.facebook.com" target="_blank">
         <img src="/img/facebook_logo.png" class="w-7" alt="facebook" />
@@ -12,13 +12,13 @@
       </a>
     </div>
   </div>
-  <span>
+  <span class="flex md:block">
     <div class="flex flex-row justify-end mx-10 mb-2 font-medium">
       © Copyright 2023 RetroPGF Hub. All rights reserved.
     </div>
   </span>
-  <div class="flex flex-row justify-end mx-10 mb-4 text-[13px]">
-    <div>
+  <div class="flex flex-row md:justify-end mx-10 mb-4 text-[13px]">
+    <div class="md:flex">
       This open-source project was built by <a
         href="https://github.com/Billy19191"
         target="_blank"
