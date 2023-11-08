f5f69716944bceb32abdfdf35821cb6c81c54f5b
Billy19191 <bill.ratchanon@gmail.com>
2023-11-08 23:30:40 +0700

    feat: fetch icon + data for card retropgf3

--- src/routes/retropgf3/+page.svelte
+++ src/routes/retropgf3/+page.svelte
@@ -4,31 +4,58 @@
   import { onMount } from 'svelte'
   let fetchData: any = []
   let loading = true
-  const fetchSheet = async () => {
-    let query = encodeURIComponent('Select *')
-    let sheet = 'Metadata_full'
-    const sheetID = '1INxDsfVREhpo5J-558VfoZb0MmbvxpoVyuMzTNuVdLw'
-    let data: any = await fetch(
-      `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheet}&tq=${query}`
-    )
-    data = await data.text()
-    let jsonData = await JSON.parse(data.slice(47, -2))
-    await jsonData.table.rows.forEach(async (row: any) => {
-      let obj: object = {}
-      row.c.forEach((cell: any, index: number) => {
-        if (cell) {
-          if (cell.v) {
-            obj[jsonData.table.cols[index].label] = cell.v
-          }
-        }
-      })
-      fetchData.push(obj)
-    })
+  let showCard = 20
+
+  const shuffle = (array: []) => {
+    let currentIndex = array.length,
+      randomIndex
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+    }
+    return array
   }
+
+  const fetchSheet = async () => {
+    try {
+      let query = encodeURIComponent('Select *')
+      let sheet = 'Metadata_full'
+      const sheetID = '13ihSoZycgH2h6ZvlARuj7q2zPvGtBqQ8LoNrJemTUU4'
+      let data: any = await fetch(
+        `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheet}&tq=${query}`
+      )
+      data = await data.text()
+      let jsonData = await JSON.parse(data.slice(47, -2))
+      await jsonData.table.rows.forEach(async (row: any) => {
+        let obj: object = {}
+        row.c.forEach((cell: any, index: number) => {
+          if (cell) {
+            if (cell.v) {
+              obj[jsonData.table.cols[index].label] = cell.v
+            }
+          }
+        })
+        fetchData.push(obj)
+      })
+    } catch (err) {
+      console.log(err)
+    } finally {
+      shuffle(fetchData)
+    }
+  }
+
   onMount(async () => {
     await fetchSheet()
     loading = false
   })
+
+  const loadMore = () => {
+    showCard += 20
+  }
 </script>
 
 <Alertbar />
@@ -59,10 +86,18 @@
   </div>
   {#if !loading}
     <div class="flex flex-row flex-wrap justify-center">
-      {#each fetchData as data}
+      {#each fetchData.slice(0, showCard) as data}
         <Card {data} />
         <!-- <div>Array {fetchData.length}</div> -->
       {/each}
     </div>
+    {#if fetchData.length > showCard}
+      <div class="flex flex-row justify-center">
+        <button
+          class="bg-black px-4 py-2 rounded-lg w-fit text-white font-base"
+          on:click={loadMore}>Show More</button
+        >
+      </div>
+    {/if}
   {/if}
 </div>
