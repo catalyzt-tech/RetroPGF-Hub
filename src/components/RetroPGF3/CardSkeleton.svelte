85960b31bf6ee4c08a8ce2e94638f167f571feec
Billy19191 <bill.ratchanon@gmail.com>
2023-11-09 00:03:20 +0700

    feat: search bar project retropgf3

---
+++ src/components/RetroPGF3/CardSkeleton.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<div
+  class="flex flex-col relative w-72 m-5 px-4 py-2 bg-white border-black border-2 rounded-2xl animate-pulse"
+>
+  <div class="absolute right-3 top-3">
+    <div
+      class=" text-sm font-medium bg-[#ff8e8e] border-2 border-[#ff8e8e] text-white w-fit rounded-lg px-10 py-3"
+    />
+  </div>
+  <div class="mt-5 mb-3 w-16 h-16 rounded-xl bg-gray-200" />
+  <a
+    class="text-lg font-semibold bg-gray-200 h-2.5 rounded-lg"
+    target="_blank"
+  />
+  <div class="text-sm text-gray-500 mt-2 bg-gray-200 h-2.5 rounded-lg" />
+  <div class="text-sm text-gray-500 mt-2 bg-gray-200 h-2.5 rounded-lg" />
+  <div class="text-sm text-gray-500 mt-2 bg-gray-200 h-2.5 rounded-lg" />
+  <div class="flex-grow" />
+  <div class="text-sm mt-3">
+    <div class="font-medium bg-gray-300 h-2.5 w-24 rounded-lg" />
+    <div class="mt-3 bg-gray-200 h-2.5 w-full rounded-lg" />
+  </div>
+
+  <div class="text-sm mt-2">
+    <div class="font-medium bg-gray-300 h-2.5 w-24 rounded-lg" />
+    <div class="mt-3 bg-gray-200 h-2.5 w-full rounded-lg" />
+  </div>
+
+  <div class="flex justify-center my-3" />
+</div>
