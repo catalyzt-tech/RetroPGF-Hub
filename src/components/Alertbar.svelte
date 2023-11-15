c76e28b480ae70387e05eeea7f12e50ab8c436f9
Billy19191 <bill.ratchanon@gmail.com>
2023-11-15 20:57:58 +0700

    chore: change text in alert bar

--- src/components/Alertbar.svelte
+++ src/components/Alertbar.svelte
@@ -1,4 +1,4 @@
 <div class=" bg-[#f03b3b] text-white text-center py-3 px-8 text-sm">
-  <div class="font-medium">RetroPGF3 Application is closed!</div>
-  <div>We are in review application period.</div>
+  <div class="font-medium">RetroPGF3 Voting is now live!</div>
+  <div>Badgeholders are going to allocate 30m OP to public goods.</div>
 </div>
