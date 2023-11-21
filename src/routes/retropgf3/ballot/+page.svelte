b27c01ce8512b3222239f68e4ba814a9117dfc56
omahs <73983677+omahs@users.noreply.github.com>
2023-11-21 12:50:28 +0100

    fix typos

--- src/routes/retropgf3/ballot/+page.svelte
+++ src/routes/retropgf3/ballot/+page.svelte
@@ -11,8 +11,8 @@
     </h1>
     <div class="flex justify-center my-3">
       <div class="text-[20px] max-w-[50em] text-base text-center">
-        This chart show all the votes from the RetroPGF 3 ballot. The size of
-        bubble depend on the bollot count. The bigger the bubble, the more votes
+        This chart shows all the votes from the RetroPGF 3 ballot. The size of
+        bubble depends on the ballot count. The bigger the bubble, the more votes
         it has. The category is defined by the color of the bubble. Mouse over
         each bubble to see the name of the project and the ballot count.
       </div>
