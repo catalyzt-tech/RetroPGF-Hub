ff01164d98ccec8d9a9781cd71e35975e2ba618f
Billy19191 <bill.ratchanon@gmail.com>
2023-12-07 01:33:44 +0700

    feat: voting end countdown

--- src/components/Alertbar.svelte
+++ src/components/Alertbar.svelte
@@ -1,4 +1,19 @@
+<script>
+  let countdownString = '0d 0h 0m 0s'
+  const countdownFunc = () => {
+    const now = Math.floor(new Date().getTime() / 1000)
+    const votingEnd = 1701975600
+    const countdown = votingEnd - now
+    countdownString = `${Math.floor(countdown / 86400)}d ${Math.floor(
+      (countdown % 86400) / 3600
+    )}h ${Math.floor((countdown % 3600) / 60)}m ${countdown % 60}s`
+  }
+  countdownFunc()
+  setInterval(countdownFunc, 1000)
+</script>
+
 <div class=" bg-[#f03b3b] text-white text-center py-3 px-8 text-sm">
-  <div class="font-medium">RetroPGF3 Voting is now live!</div>
-  <div>Badgeholders are going to allocate 30m OP to public goods.</div>
+  <div class="font-medium">RetroPGF3 Voting is ending in {countdownString}</div>
+  <div>30m OP to public goods.</div>
+  <div></div>
 </div>
