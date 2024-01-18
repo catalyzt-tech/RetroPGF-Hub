47f32d9cdb85aed7945337b02a5b640cda846ce9
Billy19191 <bill.ratchanon@gmail.com>
2024-01-19 01:23:09 +0700

    feat: draft project idea board

---
+++ src/components/ProjectIdea/Card.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<div class="w-36 border-2 border-black rounded">Hello</div>
---
+++ src/routes/projectidea/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Card from '@/components/ProjectIdea/Card.svelte'
+  import { onMount } from 'svelte'
+  let issueData = []
+  const syncData = async () => {
+    const data = await fetch('/data/project-idea/cleanIssue.json')
+    issueData = await data.json()
+    console.log(issueData[0])
+  }
+  onMount(() => {
+    syncData()
+  })
+</script>
+
+<div>
+  <div class="text-center text-xl font-semibold">
+    Optimism Ecosystem Contributions
+  </div>
+  <div>{issueData.length}</div>
+  <Card></Card>
+</div>
