d931f55a8e5155124d0cad147c44055817ae670d
Billy19191 <bill.ratchanon@gmail.com>
2024-03-15 19:43:47 +0700

    feat: migrate to  next.js

--- src/lib/axios.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const Axios = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_Backend_URL_DEV : import.meta.env.VITE_Backend_URL_PROD,
-    withCredentials: true
-});
