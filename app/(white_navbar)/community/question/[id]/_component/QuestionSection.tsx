a162fa127ba04c6ccb765a1e1dbe3b8d33c9916a
tgrziminiar <mixzazazaza1212@gmail.com>
2024-04-03 19:37:49 +0700

    add ui community question page, add some component for reuseable for question and project

--- app/(white_navbar)/community/question/[id]/_component/QuestionSection.tsx
+++ app/(white_navbar)/community/question/[id]/_component/QuestionSection.tsx
@@ -1,33 +1,35 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import Pin from '@carbon/icons-react/lib/Pin';
+import { useRef } from "react";
+import toast from "react-hot-toast";
 import Favorite from "@carbon/icons-react/lib/Favorite";
 import Chat from "@carbon/icons-react/lib/Chat";
 import InputRef from "@/app/component/Input/InputRef";
-import { useRef } from "react";
-import toast from "react-hot-toast";
 import Comment from "@/app/component/Comment/Comment";
+import Image from "next/image";
+import Help from "@carbon/icons-react/lib/Help";
 
-export default function ProjectSection({
+export default function QuestionSection({
 
 }: {
 
-}) {
+    }) {
 
     const textComment = useRef<HTMLInputElement>(null);
 
-    async function handleCreateComment(e:React.FormEvent<HTMLFormElement | HTMLButtonElement> ) {
+    async function handleCreateComment(e: React.FormEvent<HTMLFormElement | HTMLButtonElement>) {
         e.preventDefault();
 
-        if(!textComment.current?.value) {
+        if (!textComment.current?.value) {
             toast.error('You need to write some text first!')
 
         }
     }
+
     async function handleFavoriteProject(e:React.FormEvent<HTMLFormElement | HTMLButtonElement>) {
         
     }
+
     async function handleFavoriteComment(e:React.FormEvent<HTMLFormElement | HTMLButtonElement>) {
         
     }
@@ -36,35 +38,22 @@ export default function ProjectSection({
     }
 
     return (
+
         <div className="flex flex-col gap-8">
-            {/* Project Detail */}
+
+            {/* Question Detail */}
             <div className="flex flex-col gap-4 border-b border-gray-200 md:border rounded-lg p-2 md:p-6 bg-white">
-                <div className="flex gap-1.5 bg-gray-100 px-2 py-1 rounded-2xl w-fit mt-4 md:mt-0">
-                    <Pin size={20} className="fill-gray-500" />
-                    <p className="text-sm font-medium text-gray-700">Feedback Request</p>
-                </div>
-                <div className="flex flex-wrap gap-3">
-                    <Image
-                        src={"/random/metamask.png"}
-                        alt="avatar image"
-                        width={72}
-                        height={72}
-                        className="rounded-[4rem]"
-                    />
-                    <div className="flex flex-col gap-1">
-                        <p
-                            className="text-2xl font-semibold text-gray-800">
-                            Metamask
-                        </p>
-                        <div className="px-2 py-1 rounded-2xl bg-sky-50 cursor-pointer hover:bg-sky-100">
-                            <p className="text-sky-600 text-xs font-medium">Developer Ecosystem</p>
-                        </div>
-                    </div>
+                <div className="flex gap-1.5 bg-gray-100 px-2 py-1 rounded-2xl w-fit">
+                    <Help size={20} className="fill-gray-500" />
+                    <p className="text-sm font-medium text-gray-700">Question</p>
                 </div>
                 <p
-                    className="text-base font-normal text-gray-600 mb-4"
+                    className="textt-2xl font-semibold text-gray-800 "
+                >Where do i even start with Web3</p>
+                <p
+                    className="text-base font-normal text-gray-600 line-clamp-2 mb-4"
                 >
-                    A decentralized marketplace for buying and selling music NFTs. We envision a world where artists have more control over their distribution, revenue streams, and can directly connect with fans in new ways. Our platform features easy minting, transparent royalty structures, and we're exploring ways to incorporate fan-driven rewards and exclusive content tied to NFT ownership.
+                    Okay, I want to get into this Web3 thing, but it feels like everyone's speaking a different language. Are there any newbie-friendly places to learn without getting overwhelmed?
                 </p>
 
                 <hr className="border-gray-100" />
@@ -100,29 +89,29 @@ export default function ProjectSection({
                 </div>
 
             </div>
-            {/* End Project Detail */}
+            {/* End Question Detail */}
 
 
 
 
             {/* Create Comment */}
-            <form 
-            onSubmit={handleCreateComment}
-            className="flex flex-col gap-4 p-2 md:p-0">
+            <form
+                onSubmit={handleCreateComment}
+                className="flex flex-col gap-4 p-2 md:p-0">
 
                 <InputRef
                     type="text"
                     placeholder="Add a comment"
                     ref={textComment}
                     className="border border-gray-200 placeholder-slate-600 bg-white text-slate-800 focus:ring focus:ring-primaryRed w-full px-5 py-3 rounded-md min-h-[40px]"
-                    />
+                />
 
                 <button
-                className="self-end py-3 px-7 w-fit rounded-lg bg-gray-800 hover:bg-gray-800/90"
+                    className="self-end py-3 px-7 w-fit rounded-lg bg-gray-800 hover:bg-gray-800/90"
                 >
                     <p className="text-white text-base font-semibold text-center self-center">Comment</p>
                 </button>
-                
+
             </form>
             {/* End Create Comment */}
 
---
+++ app/component/BreadCump/BreadCumpCommunity.tsx
@@ -0,0 +1,21 @@
+import ChevronRight from "@carbon/icons-react/lib/ChevronRight";
+
+export default function BreadCumpCommunity({
+
+}:{
+    
+})  {
+
+return (
+
+    <> 
+     <div className="flex gap-2 p-2 md:p-0">
+            <p className="text-xs font-normal text-gray-800"> Community </p>
+                <ChevronRight />
+            <p className="text-xs font-normal text-gray-500"> Post </p>
+        </div>
+    </>
+
+    )
+
+}
