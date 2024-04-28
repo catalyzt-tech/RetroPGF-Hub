6977fa65c2ee464317e1fcfa884cbdfbdf4df25a
Billy19191 <bill.ratchanon@gmail.com>
2024-04-29 00:22:42 +0700

    feat: retropgf 3 sign-up

--- app/component/History/RetroPGF3/RPGF3History.tsx
+++ app/component/History/RetroPGF3/RPGF3History.tsx
@@ -101,61 +101,299 @@ const RPGF3History = () => {
           <li className="mb-6 ml-6">
             <div className="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white" />
             <time className="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500">
-              7 - 21st February 2023
+              23rd October - 5th November 2023
             </time>
             <div className="text-md font-semibold text-gray-700">
-              Project Profile Creation
+              Application Review & Appeal
             </div>
             <p className="text-gray-600">
-              Projects had to create a profile where they were asked for general
-              information, as well as a description of their project and its
-              impact. Information provided by Projects can be viewed on the
-              <a href="/archive" className="text-[#ff1818] hover:underline">
-                archive page
-              </a>
-              .
+              An Application Review Process was initiated to address any
+              violations of RetroPGF Application Rules, allowing individuals to
+              submit reports on applications that were not compliant. More than
+              1,000 applications were flagged for breaching these rules.
             </p>
           </li>
           <li className="mb-6 ml-6">
             <div className="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white" />
             <time className="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500">
-              7 - 21st March 2023
+              6th November - 7th December 2023
             </time>
             <div className="text-md font-semibold text-gray-700">Voting</div>
             <p className="text-gray-600">
-              Badgeholders were provided with a{' '}
-              <a
-                href="https://oplabs.notion.site/Optimism-RetroPGF-2-Badgeholder-Manual-11ef6cd63c254258a32246289f45925c#de8c93098c854a62951d721fb95bf51c"
-                className="text-[#ff1818] hover:underline"
-              >
-                badgeholder manual
-              </a>
-              and asked to evaluate and vote on nominated projects via a
-              <a
-                href="https://app.deform.cc/form/85e65189-6679-4a13-908e-c42ea3cf1498/"
-                className="text-[#ff1818] hover:underline"
-              >
-                voting form
-              </a>
+              Badgeholders will vote on the projects they believe should receive
+              funding. Each badgeholder will have a certain amount of OP to
+              distribute to the projects they believe are most deserving in
+              axiom of "impact = profit".
             </p>
           </li>
           <li className="ml-6">
             <div className="absolute w-3 h-3 bg-[#ff1818] rounded-full mt-1.5 -left-1.5 border border-white" />
             <time className="mb-1 text-sm font-normal leading-none text-[#ff1818] dark:text-gray-500">
-              After April 2023
+              After January 2024
             </time>
             <div className="text-md font-semibold text-gray-700">
               Payout / Funding Distribution
             </div>
             <p className="text-gray-600">
               Projects received rewards based on their received OP allocation
-              votes from each badgeholders which can be viewed{' '}
-              <a href="/archive" className="text-[#ff1818] hover:underline">
-                here
-              </a>
+              votes from each badgeholder, celebrating their valuable work and
+              impact.
             </p>
           </li>
         </ol>
+      </section>
+      <section className="mb-10" id="scope">
+        <div className="font-semibold text-3xl text-gray-800">
+          Scope of Round
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          The third iteration of RetroPGF marks a significant milestone for the
+          Optimism Collective, as 30 million OP tokens are poised to be
+          distributed among developers, innovators, and educators who have
+          played a role in the advancement and adoption of Optimism. This round
+          underscores the Collective's dedication to acknowledging community
+          members for their contributions and cultivating a thriving ecosystem
+          that benefits all involved. At Optimism, we believe in recognizing
+          every form of contribution through Retroactive Public Goods Funding
+          (RetroPGF). Whether you're a skilled programmer crafting an Ethereum
+          execution client or a content creator spreading enthusiasm for
+          Optimism through videos, if you're making a positive impact, you're
+          welcomed!
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          We're rolling out the red carpet for Optimism artists, creators,
+          writers, builders, and all the passionate folks who make our ecosystem
+          thrive.
+        </div>
+        <ol className="list-disc ml-5 mt-5 text-gray-600">
+          <li>
+            <div className="text-gray-700 font-medium">OP Stack</div>
+            <p className="text-gray-600">
+              If you've contributed to making the OP Stack more efficient,
+              secure, resilient, or simply raised awareness about it, you're
+              eligible!
+            </p>
+          </li>
+          <li>
+            <div className="text-gray-700 font-medium">
+              Collective Governance
+            </div>
+            <p className="text-gray-600">
+              Did your work benefit governance participants within the Optimism
+              Collective or bring new faces into the governance fold? You're in!
+            </p>
+          </li>
+          <li>
+            <div className="text-gray-700 font-medium">
+              Developer Ecosystem{' '}
+            </div>
+            <p className="text-gray-600">
+              If you've had a hand in improving the lives of application
+              developers within the Optimism Collective or helped newcomers find
+              their way in, you're a RetroPGF candidate!
+            </p>
+          </li>
+          <li>
+            <div className="text-gray-700 font-medium">
+              End User Experience & Adoption
+            </div>
+            <p className="text-gray-600">
+              Perhaps you've improved the experience for end users in the
+              Optimism Collective, or you've been a champion in bringing new
+              users on board. Your impact matters!
+            </p>
+          </li>
+        </ol>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          You can select multiple categories, but try to select the categories
+          that best describe your impact! The more specific you are, the better
+          the badgeholders can understand your impact and reward you
+          accordingly.
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          RetroPGF isn't just for a select few. It's for all the amazing
+          individuals who've left their mark in these categories. So, if you've
+          made a difference, don't hesitate to submit an application and let's
+          celebrate your contributions together!
+        </div>
+        <Image
+          src="/static/historyRPGF3/retropgf3_scope.png"
+          className="my-5 rounded-lg"
+          width={1000}
+          height={400}
+          layout="responsive"
+          alt="Scope of Round"
+        />
+        <div className="text-gray-700 text-lg font-semibold mt-4">
+          How to get involved
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          If you're ready to make a difference and get involved in the third
+          round of RetroPGF, here's how you can participate:
+        </div>
+        <div className=" bg-[#ffe9eb] p-6 rounded-lg mt-4">
+          <ol className="list-disc ml-5 my-5 text-gray-600">
+            <li>
+              <div className="text-gray-700 font-medium">
+                If You're a Developer
+              </div>
+              <p className="text-gray-600">
+                Take a look at the Ecosystem Contributions board where you'll
+                find a treasure trove of ideas and projects waiting for your
+                magic touch. Who knows, these could be your ticket to RetroPGF
+                funding and making an impact!
+              </p>
+            </li>
+            <li>
+              <div className="text-gray-700 font-medium">
+                Got a Project in Mind?
+              </div>
+              <p className="text-gray-600">
+                If you're working on something that aligns with our Collective
+                Intents but need a little financial boost to kickstart it, check
+                out the guide on how to apply for a grant. We've got your back!
+              </p>
+            </li>
+            <li>
+              <div className="text-gray-700 font-medium">
+                Community Builder Extraordinaire
+              </div>
+              <p className="text-gray-600">
+                If you're all about building communities, consider exploring the
+                NERD program for support and onboarding or the Ambassador
+                program if you're into marketing and business development.
+                There's a place for you to shine!
+              </p>
+            </li>
+            <li>
+              <div className="text-gray-700 font-medium">
+                Choose Your Adventure
+              </div>
+              <p className="text-gray-600">
+                Feel like charting your own course? That's cool too! In RetroPGF
+                2, we celebrated and funded 195 incredible projects and people.
+                You can check out project page to get inspired and find your
+                unique way to contribute.
+              </p>
+            </li>
+          </ol>
+        </div>
+      </section>
+      <section className="mb-10" id="design">
+        <div className="font-semibold text-3xl text-gray-800">Round Design</div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          Retroactive Public Goods Funding is a dynamic experiment, always
+          evolving to better serve our amazing community. In this post, we're
+          taking you behind the scenes to share the nitty-gritty of RetroPGF 3's
+          design process.
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          Let's talk about what we've cooked up for Round 3. These design
+          choices are like ingredients in our recipe for success, and they come
+          with their own set of assumptions. We're super excited to test these
+          out in the upcoming Round and use your feedback to keep making
+          RetroPGF even better.
+        </div>
+        <div className="flex flex-row flex-wrap gap-6 my-5">
+          <button className="flex flex-col  flex-grow  px-8 py-5 border-2  rounded-lg ">
+            <div className="text-gray-700 font-semibold text-lg">
+              RetroPGF Sign up
+            </div>
+            <div className="mt-3 text-gray-600">
+              Gathering the right data from Projects
+            </div>
+          </button>
+          <button className="flex flex-col  flex-grow  px-8 py-5 border-2  rounded-lg ">
+            <div className="text-gray-700 font-semibold text-lg">
+              Lists Application
+            </div>
+            <div className="mt-3 text-gray-600">
+              Scaling the Evaluation of Projects
+            </div>
+          </button>
+          <button className="flex flex-col  flex-grow  px-8 py-5 border-2  rounded-lg ">
+            <div className="text-gray-700 font-semibold text-lg">
+              Impact Evaluation
+            </div>
+            <div className="mt-3 text-gray-600">Frameworks & Definitions</div>
+          </button>
+          <button className="flex flex-col  flex-grow  px-8 py-5 border-2  rounded-lg ">
+            <div className="text-gray-700 font-semibold text-lg">
+              Voting Application
+            </div>
+            <div className="mt-3 text-gray-600">
+              How badgeholders cast their vote allocation
+            </div>
+          </button>
+        </div>
+        <div className="text-gray-700 text-lg font-semibold mt-4">
+          RetroPGF Sign up
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          We're on a mission to make Retroactive Public Goods Funding (RetroPGF)
+          even better by helping projects showcase their impact and profit more
+          effectively. Our goal is to empower projects to quantify their
+          contributions and give badgeholders the insights they need.
+        </div>
+        <Image
+          src="/static/historyRPGF3/retropgf3_design_1.jpg"
+          className="my-5 rounded-lg"
+          width={1000}
+          height={400}
+          layout="responsive"
+          alt="RetroPGF Sign up"
+        />
+        <div className="text-gray-600 text-base font-normal mt-4">
+          Our new feature, the Optimist Profile, is our first step towards this
+          exciting future. It allows projects to share their impact and profit
+          metrics, along with references to data sources that support their
+          claims. We believe that projects are the experts in measuring their
+          own impact, and we want to give them the tools to do it.
+        </div>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          In RetroPGF 3, we're introducing a revamped sign-up process with a
+          focus on gathering high-quality data:
+        </div>
+        <ol className="list-disc ml-5 mt-5 text-gray-600">
+          <li>
+            <div className="text-gray-700 font-medium">
+              More Detailed Questions
+            </div>
+            <p className="text-gray-600">
+              We've added thoughtful questions that align with the impact
+              evaluation process, helping projects provide a comprehensive view
+              of their work.
+            </p>
+          </li>
+          <li>
+            <div className="text-gray-700 font-medium">
+              Standardized Data Sources
+            </div>
+            <p className="text-gray-600">
+              Now, projects can input relevant standardized external data
+              sources like Github repositories or on-chain contracts. This
+              allows us to create more powerful analytical tools for evaluation.
+            </p>
+          </li>
+          <li>
+            <div className="text-gray-700 font-medium">
+              Self-Reported Impact Metrics
+            </div>
+            <p className="text-gray-600">
+              Projects can share their impact metrics and back them up with
+              references to external data sources. This helps us identify
+              standardized metrics and evaluation frameworks for the future.
+            </p>
+          </li>
+        </ol>
+        <div className="text-gray-600 text-base font-normal mt-4">
+          Good news for projects! We've simplified the process. No More
+          Nominations, Just Direct Sign-up. This makes the experience smoother,
+          reducing the steps from nomination and sign-up to a single,
+          straightforward process. Join us in making RetroPGF even better, one
+          step at a time! Together, we're building a brighter future for project
+          evaluation!
+        </div>
       </section>
     </>
   )
