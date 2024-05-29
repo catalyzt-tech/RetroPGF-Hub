f4ed26baca412e72183c54ed3435c1e1994ee870
Billy191 <bill.ratchanon@gmail.com>
2024-05-29 17:39:10 +0700

    fix: incorrect search dependencies

--- app/(transparent_navbar)/explore/Cpage.tsx
+++ app/(transparent_navbar)/explore/Cpage.tsx
@@ -1,119 +1,102 @@
-"use client"
+'use client'
 
-import { useMemo, useState } from "react"
-import CardRound1 from "./_component/CardRound1"
-import CardRound2 from "./_component/CardRound2"
-import CardRound3 from "./_component/CardRound3"
-import CategorySection from "./_component/CategorySection"
-import InputSearchExplore from "./_component/InputSearchExplore"
-import { RetroRound3 } from "@/app/(white_navbar)/explore/RetroPGF3/RetroType3"
-import { RetroRound2 } from "@/app/(white_navbar)/explore/RetroPGF2/RetroType2"
-import { RetroRound1 } from "@/app/(white_navbar)/explore/RetroPGF1/RetroType1"
+import { useMemo, useState } from 'react'
+import CardRound1 from './_component/CardRound1'
+import CardRound2 from './_component/CardRound2'
+import CardRound3 from './_component/CardRound3'
+import CategorySection from './_component/CategorySection'
+import InputSearchExplore from './_component/InputSearchExplore'
+import { RetroRound3 } from '@/app/(white_navbar)/explore/RetroPGF3/RetroType3'
+import { RetroRound2 } from '@/app/(white_navbar)/explore/RetroPGF2/RetroType2'
+import { RetroRound1 } from '@/app/(white_navbar)/explore/RetroPGF1/RetroType1'
 
 export default function Cpage({
-    round1,
-    round2,
-    round3,
-    cateRound2,
-    cateRound3
+  round1,
+  round2,
+  round3,
+  cateRound2,
+  cateRound3,
 }: {
-    round3: RetroRound3[];
-    round2: RetroRound2[];
-    round1: RetroRound1[];
-    cateRound2: Map<string, number>;
-    cateRound3: Map<string, number>
-
+  round3: RetroRound3[]
+  round2: RetroRound2[]
+  round1: RetroRound1[]
+  cateRound2: Map<string, number>
+  cateRound3: Map<string, number>
 }) {
+  const [search, setSearch] = useState<string>('')
 
-    const [search, setSearch] = useState<string>("");
+  const round3Data = useMemo(() => {
+    // console.log(round3.length)
+    return round3.filter((item) => {
+      const searchCondition =
+        search !== ''
+          ? typeof item.displayName === 'string' &&
+            item.displayName.toLowerCase().includes(search.toLowerCase())
+          : true
+      return searchCondition
+    })
+  }, [round3, search])
 
+  const round2Data = useMemo(() => {
+    // console.log(round3.length)
+    return round2.filter((item) => {
+      const searchCondition =
+        search !== ''
+          ? typeof item['Project Name'] === 'string' &&
+            item['Project Name'].toLowerCase().includes(search.toLowerCase())
+          : true
+      return searchCondition
+    })
+  }, [round2, search])
 
-    const round3Data = useMemo(() => {
-        // console.log(round3.length)
-        return round3.filter((item) => {
-            const searchCondition =
-                search !== ''
-                    ? typeof item.displayName === 'string' &&
-                    item.displayName.toLowerCase().includes(search.toLowerCase())
-                    : true
-            return (
-                searchCondition
-            )
-        })
-    }, [round3, search])
+  const round1Data = useMemo(() => {
+    // console.log(round3.length)
+    return round1.filter((item) => {
+      const searchCondition =
+        search !== ''
+          ? typeof item['Project Name'] === 'string' &&
+            item['Project Name'].toLowerCase().includes(search.toLowerCase())
+          : true
+      return searchCondition
+    })
+  }, [round1, search])
 
-    const round2Data = useMemo(() => {
-        // console.log(round3.length)
-        return round2.filter((item) => {
-            const searchCondition =
-                search !== ''
-                    ? typeof item["Project Name"] === 'string' &&
-                    item["Project Name"].toLowerCase().includes(search.toLowerCase())
-                    : true
-            return (
-                searchCondition
-            )
-        })
-    }, [round3, search])
-
-    const round1Data = useMemo(() => {
-        // console.log(round3.length)
-        return round1.filter((item) => {
-            const searchCondition =
-                search !== ''
-                    ? typeof item["Project Name"] === 'string' &&
-                    item["Project Name"].toLowerCase().includes(search.toLowerCase())
-                    : true
-            return (
-                searchCondition
-            )
-        })
-    }, [round3, search])
-
-    return (
-
-        <>
-            <div className="relative max-h-[19rem] min-h-[19rem]">
-                <div className="bg-gradient-to-br from-[#FFFEEA] to-100% via-[#FFDAAF] to-[#FFAB9F] w-full absolute -top-[4.5rem] left-0 h-[23.5rem]">
-                    <div className="mt-[4.5rem]" />
-                    <div className="px-4 md:px-0 py-12 md:py-16  flex flex-col justify-center items-center font-rubik h-[19rem] w-full">
-                        <h1 className="mb-3 md:mb-7 text-3xl md:text-5xl font-semibold text-gray-800">Explore Project Ideas</h1>
-                        <p className="mb-8 text-lg font-normal text-gray-900">Find inspiration from all RetroPGF projects</p>
-                        <div className="w-full min-[350px]:w-[20rem] lg:w-[26.25rem] ">
-                            <InputSearchExplore
-                                search={search}
-                                setSearch={setSearch}
-                            />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className=" animate-slowfade">
+      <div className="relative max-h-[19rem] min-h-[19rem]">
+        <div className="bg-gradient-to-br from-[#FFFEEA] to-100% via-[#FFDAAF] to-[#FFAB9F] w-full absolute -top-[4.5rem] left-0 h-[23.5rem]">
+          <div className="mt-[4.5rem]" />
+          <div className="px-4 md:px-0 py-12 md:py-16  flex flex-col justify-center items-center font-rubik h-[19rem] w-full">
+            <h1 className="mb-3 md:mb-7 text-3xl md:text-5xl font-semibold text-gray-800">
+              Explore Project Ideas
+            </h1>
+            <p className="mb-8 text-lg font-normal text-gray-900">
+              Find inspiration from all RetroPGF projects
+            </p>
+            <div className="w-full min-[350px]:w-[20rem] lg:w-[26.25rem] ">
+              <InputSearchExplore search={search} setSearch={setSearch} />
             </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="lg:px-20 px-4 flex flex-col gap-12 mt-12">
-                <CategorySection />
+      <div className="lg:px-20 px-4 flex flex-col gap-12 mt-12">
+        <CategorySection />
 
-                <CardRound3
-                    title="RetroPGF3"
-                    round3={round3Data}
-                    cateRound3={cateRound3}
+        <CardRound3
+          title="RetroPGF3"
+          round3={round3Data}
+          cateRound3={cateRound3}
+        />
 
-                />
+        <CardRound2
+          title="RetroPGF2"
+          cateRound2={cateRound2}
+          round2={round2Data}
+        />
 
-                <CardRound2
-                    title="RetroPGF2"
-                    cateRound2={cateRound2}
-                    round2={round2Data}
-                />
-
-                <CardRound1
-                    title="RetroPGF1"
-                    round1={round1Data}
-
-                />
-
-            </div>
-        </>
-
-    )
-
+        <CardRound1 title="RetroPGF1" round1={round1Data} />
+      </div>
+    </div>
+  )
 }
--- app/(transparent_navbar)/explore/page.tsx
+++ app/(transparent_navbar)/explore/page.tsx
@@ -1,70 +1,80 @@
-import { RetroRound3 } from "@/app/(white_navbar)/explore/RetroPGF3/RetroType3";
-import { RetroRound2 } from "@/app/(white_navbar)/explore/RetroPGF2/RetroType2";
-import { RetroRound1 } from "@/app/(white_navbar)/explore/RetroPGF1/RetroType1";
-import { getJsonRound1 } from "@/app/(white_navbar)/explore/RetroPGF1/page";
-import { getJsonRound2 } from "@/app/(white_navbar)/explore/RetroPGF2/page";
-import { getJsonRound3 } from "@/app/(white_navbar)/explore/RetroPGF3/page";
-import Cpage from "./Cpage";
+import { RetroRound3 } from '@/app/(white_navbar)/explore/RetroPGF3/RetroType3'
+import { RetroRound2 } from '@/app/(white_navbar)/explore/RetroPGF2/RetroType2'
+import { RetroRound1 } from '@/app/(white_navbar)/explore/RetroPGF1/RetroType1'
+import { getJsonRound1 } from '@/app/(white_navbar)/explore/RetroPGF1/page'
+import { getJsonRound2 } from '@/app/(white_navbar)/explore/RetroPGF2/page'
+import { getJsonRound3 } from '@/app/(white_navbar)/explore/RetroPGF3/page'
+import Cpage from './Cpage'
 
+export async function getAllRound(
+  limit: number
+): Promise<{
+  round1: RetroRound1[]
+  round2: RetroRound2[]
+  round3: RetroRound3[]
+  cateRound3: Map<string, number>
+  cateRound2: Map<string, number>
+}> {
+  const round1 = (await getJsonRound1()).sort((a, b) => {
+    return b.allocation - a.allocation
+  })
 
-export async function getAllRound(limit:number): Promise<{round1:RetroRound1[], round2:RetroRound2[], round3:RetroRound3[], cateRound3: Map<string, number>, cateRound2: Map<string, number>}> {
-    
-    const round1 = (await getJsonRound1()).sort((a, b) => {
-        return b.allocation - a.allocation
-    });
+  const round2 = await getJsonRound2()
 
-    const round2 = (await getJsonRound2());
+  const round3 = await getJsonRound3()
 
-    const round3 = (await getJsonRound3());
+  const cateRound2Counter = new Map<string, number>()
+  const cateRound3Counter = new Map<string, number>()
 
-    const cateRound2Counter = new Map<string, number>();
-    const cateRound3Counter = new Map<string, number>();
+  round2.forEach((project) => {
+    const cateRound2 = project.Category
+    if (cateRound2) {
+      if (cateRound2Counter.has(cateRound2)) {
+        cateRound2Counter.set(
+          cateRound2,
+          cateRound2Counter.get(cateRound2)! + 1
+        )
+      } else {
+        cateRound2Counter.set(cateRound2, 1)
+      }
+    }
+  })
 
-    round2.forEach(project => {
-        const cateRound2 = project.Category;
-        if (cateRound2) {
-            if (cateRound2Counter.has(cateRound2)) {
-                cateRound2Counter.set(cateRound2, cateRound2Counter.get(cateRound2)! + 1);
-            } else {
-                cateRound2Counter.set(cateRound2, 1);
-            }
-        }
-    });
+  round3.forEach((project) => {
+    const cateRound3 = project['New Main-Category']
+    if (cateRound3) {
+      if (cateRound3Counter.has(cateRound3)) {
+        cateRound3Counter.set(
+          cateRound3,
+          cateRound3Counter.get(cateRound3)! + 1
+        )
+      } else {
+        cateRound3Counter.set(cateRound3, 1)
+      }
+    }
+  })
 
-    round3.forEach(project => {
-        const cateRound3 = project["New Main-Category"];
-        if (cateRound3) {
-            if (cateRound3Counter.has(cateRound3)) {
-                cateRound3Counter.set(cateRound3, cateRound3Counter.get(cateRound3)! + 1);
-            } else {
-                cateRound3Counter.set(cateRound3, 1);
-            }
-        }
-    });
-
-
-    return {round1:round1.slice(0, limit), round2:round2.slice(0, limit), round3:round3.slice(0, limit), cateRound3:cateRound3Counter, cateRound2:cateRound2Counter};
+  return {
+    round1: round1.slice(0, limit),
+    round2: round2.slice(0, limit),
+    round3: round3.slice(0, limit),
+    cateRound3: cateRound3Counter,
+    cateRound2: cateRound2Counter,
+  }
 }
 
+export default async function page({}: {}) {
+  const { round1, round2, round3, cateRound2, cateRound3 } = await getAllRound(
+    20
+  )
 
-export default async function page({
-
-}: {
-
-}) {
-
-    const { round1, round2, round3, cateRound2, cateRound3 } = await getAllRound(20)
-
-    return (
-        <Cpage
-        cateRound2={cateRound2}
-        cateRound3={cateRound3}
-        round1={round1}
-        round2={round2}
-        round3={round3}
-        />
-
-
-    )
-
+  return (
+    <Cpage
+      cateRound2={cateRound2}
+      cateRound3={cateRound3}
+      round1={round1}
+      round2={round2}
+      round3={round3}
+    />
+  )
 }
