32e9987bccfaeefc1ec3f5da9eb84398d7f242d1
Billy19191 <bill.ratchanon@gmail.com>
2024-06-18 23:18:35 +0700

    feat: chainId logo & set up mock github stats

--- app/(white_navbar)/explore/RetroPGF4/[name]/_component/_Project/ContractSection.tsx
+++ app/(white_navbar)/explore/RetroPGF4/[name]/_component/_Project/ContractSection.tsx
@@ -1,5 +1,6 @@
 import { iRetroPGF4Project } from '../../../RetroType4'
 import { mapChainId } from '@/app/lib/utils'
+import Image from 'next/image'
 export default function GithubSection({
   data,
   contractRef,
@@ -21,17 +22,24 @@ export default function GithubSection({
       ref={contractRef}
       className="flex flex-col gap-6 bg-white rounded-lg p-2 lg:p-6"
     >
-      <h3 className="text-3xl font-semibold">Contracts</h3>
+      <h3 className="text-3xl font-semibold">Contract Addresses</h3>
       <hr className="border-t-gray-100" />
       {data.contracts.map((item, i) => (
         <div className="flex flex-col gap-1" key={i}>
-          <div className="flex flex-wrap gap-3 items-center">
-            <p className="mb-1 text-base font-normal text-gray-500">
+          <div className="flex flex-row flex-wrap gap-3 items-center ">
+            <Image
+              src={`/static/superchainLogo/${mapChainId(item.chainId)}.webp`}
+              alt="chain"
+              className="rounded-full"
+              width={24}
+              height={24}
+            />
+            <p className="text-base font-normal text-gray-500">
               {item.address}
             </p>
-            <p className="mb-1 text-base font-semibold text-gray-600">
+            <p className="text-base font-semibold text-gray-600">
               {/* Chain ID: {item.chainId} */}
-              {mapChainId(item.chainId) || ''}
+              {/* {mapChainId(item.chainId) || ''} */}
             </p>
           </div>
         </div>
