77e510a18f88e6f78a0be81a58cbbec8143abac2
Billy19191 <bill.ratchanon@gmail.com>
2024-06-21 23:48:18 +0700

    fix: cleanname params for all round

--- app/(white_navbar)/explore/RetroPGF3/[name]/Cpage.tsx
+++ app/(white_navbar)/explore/RetroPGF3/[name]/Cpage.tsx
@@ -1,56 +1,45 @@
-"use client"
-import { useRef } from "react";
-import ProjectDetailSection from "./_component/_Project/ProjectDetailSection";
-import OverviewSection from "./_component/_Project/OverviewSection";
-import ImpactSection from "./_component/_Project/ImpactSection";
-import FundingSection from "./_component/_Project/FundingSection";
-import ContributionSection from "./_component/_Project/ContributionSection";
-import { RetroRound3 } from "../RetroType3";
-import ScrollSpy from "@/app/component/ScrollSpy";
+'use client'
+import { useRef } from 'react'
+import ProjectDetailSection from './_component/_Project/ProjectDetailSection'
+import OverviewSection from './_component/_Project/OverviewSection'
+import ImpactSection from './_component/_Project/ImpactSection'
+import FundingSection from './_component/_Project/FundingSection'
+import ContributionSection from './_component/_Project/ContributionSection'
+import { RetroRound3 } from '../RetroType3'
+import ScrollSpy from '@/app/component/ScrollSpy'
 
+export default function Cpage({ data }: { data: RetroRound3 }) {
+  const overViewRef = useRef<HTMLElement | null>(null)
+  const contributionRef = useRef<HTMLElement | null>(null)
+  const impactRef = useRef<HTMLElement | null>(null)
+  const fundingRef = useRef<HTMLElement | null>(null)
+  const sections = [
+    { name: 'Overview', ref: overViewRef },
+    { name: 'Contribution', ref: contributionRef },
+    { name: 'Impact', ref: impactRef },
+    { name: 'Funding Sources', ref: fundingRef },
+  ]
+  return (
+    <div className="flex mt-8 gap-10">
+      {/* Scroll Spy */}
 
-export default function Cpage({
-    data
-}:{
-    data: RetroRound3   
-})  {
+      <div className="lg:flex-grow flex flex-col gap-4 lg:gap-8">
+        <section
+          className="w-full h-full flex flex-col gap-4 lg:gap-8 lg:flex-grow"
+          id="Overview"
+          ref={overViewRef}
+        >
+          <ProjectDetailSection data={data} />
+          <OverviewSection data={data} />
+        </section>
+        <ContributionSection data={data} contributionRef={contributionRef} />
+        <ImpactSection data={data} impactRef={impactRef} />
+        <FundingSection data={data} fundingRef={fundingRef} />
+      </div>
 
-    const overViewRef = useRef<HTMLElement | null>(null)
-    const contributionRef = useRef<HTMLElement | null>(null)
-    const impactRef = useRef<HTMLElement | null>(null)
-    const fundingRef = useRef<HTMLElement | null>(null)
-    const sections = [
-        { name: "Overview", ref: overViewRef },
-        { name: "Contribution", ref: contributionRef },
-        { name: "Impact", ref: impactRef },
-        { name: "Funding Sources", ref: fundingRef },
-      ];
-return (
-
-    <div className="flex mt-8 gap-10"> 
-    {/* Scroll Spy */}
-       
-        <div className="lg:flex-grow flex flex-col gap-4 lg:gap-8">
-            <section className="w-full h-full flex flex-col gap-4 lg:gap-8 lg:flex-grow" id="Overview" ref={overViewRef}>
-                <ProjectDetailSection
-                data={data}
-                />
-                <OverviewSection
-                data={data}
-                />
-            </section>
-            <ContributionSection data={data} contributionRef={contributionRef} />
-            <ImpactSection data={data} impactRef={impactRef} />
-            <FundingSection data={data} fundingRef={fundingRef} />
-        </div>
-
-        <div className="hidden lg:block max-w-72 min-w-72 rounded-lg ">
-            <ScrollSpy
-            sections={sections}
-            />
-        </div>
+      <div className="hidden lg:block max-w-72 min-w-72 rounded-lg ">
+        <ScrollSpy sections={sections} />
+      </div>
     </div>
-
-    )
-
+  )
 }
