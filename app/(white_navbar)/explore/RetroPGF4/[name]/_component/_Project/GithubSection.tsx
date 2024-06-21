c66f111751e5a315f94d9130eeaf6e5c5757746e
wit03 <witjarukit@gmail.com>
2024-06-21 18:41:38 +0700

    fix: unnessesary github section gap

--- app/(white_navbar)/explore/RetroPGF4/[name]/_component/_Project/GithubSection.tsx
+++ app/(white_navbar)/explore/RetroPGF4/[name]/_component/_Project/GithubSection.tsx
@@ -29,10 +29,9 @@ export default function GithubSection({
     <section
       id="Github"
       ref={githubRef}
-      className="flex flex-col gap-6 bg-white rounded-lg p-2 lg:p-6"
+      className="flex flex-col bg-white rounded-lg p-2 lg:p-6"
     >
-      <h3 className="text-2xl font-semibold">Github</h3>
-      <hr className="border-t-gray-100" />
+      <h3 className="text-2xl font-semibold mb-4">Github</h3>
       <div className="flex flex-row flex-wrap gap-4">
         {data.github.length != 0 &&
           data.github.map((item, i) => (
