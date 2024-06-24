13f44e03e83797cd2091eb64a16cd42523759d25
wit03 <witjarukit@gmail.com>
2024-06-24 23:33:48 +0700

    feat: robots.txt and sitemap

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://retropgfhub.com/',
+    generateRobotsTxt: true,
+    sitemapSize: 1000,
+}
+  
+export default config
