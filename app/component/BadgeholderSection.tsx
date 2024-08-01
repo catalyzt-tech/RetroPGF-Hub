be2406f3eedddfb05a0ff8864bc1fad37d10fb5f
Billy19191 <bill.ratchanon@gmail.com>
2024-08-02 00:07:55 +0700

    refactor: Update BadgeholderSection to use React.FC

--- app/component/BadgeholderSection.tsx
+++ app/component/BadgeholderSection.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, { FC } from 'react'
 import BadgeholderCard from '../(white_navbar)/explore/RetroPGF4/_component/BadgeholderCard'
 import { makeRequest } from '../hook/makeRequest'
 import axios from 'axios'
 
-const BadgeholderSection = () => {
-  
+const BadgeholderSection: FC = () => {
   //waiting for data pull
-    
 
   return (
     <div className="xl:mx-10">
