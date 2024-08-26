81b03a8efea4603f6f02a90dfc39f54bc4a5d195
Billy19191 <bill.ratchanon@gmail.com>
2024-08-27 00:00:05 +0700

    refactor: improve type handling in getRealTimeRetroPGF5 function

--- app/lib/realtime.ts
+++ app/lib/realtime.ts
@@ -1,19 +1,18 @@
-import axios from "axios"
-import { RetroPGF5Response } from "../(white_navbar)/explore/RetroPGF5/RetroType5"
+import axios from 'axios'
+import { RetroPGF5Response } from '../(white_navbar)/explore/RetroPGF5/RetroType5'
 
 export const getRealTimeRetroPGF5 = async (): Promise<RetroPGF5Response> => {
-    try {
-
-      let apiUrl = process.env.NODE_ENV == "production" ? process.env.NEXT_PUBLIC_WORKER_API_BASE! : process.env.NEXT_PUBLIC_WORKER_API_DEV!
-      apiUrl = apiUrl + "/api/retropgf5-live-data/retropgf5-live-data.json"
-      const response = await axios.get<RetroPGF5Response>(apiUrl)
-      
-      const data = response.data
-      
-      return data
-      
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      throw error
-    }
+  try {
+    let apiUrl =
+      process.env.NODE_ENV == 'production'
+        ? process.env.NEXT_PUBLIC_WORKER_API_BASE!
+        : process.env.NEXT_PUBLIC_WORKER_API_DEV!
+    apiUrl = apiUrl + '/api/retropgf5-live-data/retropgf5-live-data.json'
+    const response = await axios.get<RetroPGF5Response>(apiUrl)
+    const data: RetroPGF5Response = response.data
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw error
+  }
 }
