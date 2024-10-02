8fc43992c2d29f0e1a400e576481fa68bb909073
tgrziminiar <mixzazazaza1212@gmail.com>
2024-10-02 11:52:48 +0700

    refactor: check the value before use on api call on round 5

--- app/lib/realtime.ts
+++ app/lib/realtime.ts
@@ -11,6 +11,10 @@ export const getRealTimeRetroPGF = async (): Promise<iRetroPGF5Project[]> => {
 
     const response = await axios.get<WorkerResponse>(apiUrl)
     const data: WorkerResponse = response.data
+    if (!Array.isArray(data.data) || data.data.length === 0 || !Array.isArray(data.data[0].data)) {  
+      throw new Error('Unexpected response structure');  
+    }  
+    
     return data.data[0].data
   } catch (error) {
     console.error('Error fetching data:', error)
