7cdc609d9aaa7f35ae2602f4feba0375ef9b0852
Billy19191 <bill.ratchanon@gmail.com>
2024-10-18 03:36:12 +0700

    feat: add AverageStar component

--- app/(white_navbar)/explore/RetroPGF6/[name]/_component/_Project/component/AverageStar.tsx
+++ app/(white_navbar)/explore/RetroPGF6/[name]/_component/_Project/component/AverageStar.tsx
@@ -1,15 +1,16 @@
-import { RiUserFill } from '@remixicon/react'
+import { RiStarFill } from '@remixicon/react'
 
-export default function AmountAttestation() {
+export default function AverageStar() {
   return (
     <div className="flex grow bg-slate-50 w-80 min-h-44 rounded-lg px-8 py-8">
       <div className="flex flex-col gap-2">
         <div className="flex flex-col justify-center gap-y-3 text-lg font-medium h-full">
           <div className="flex-grow"></div>
-          <RiUserFill size={30} />
-          <div className="text-md font-semibold">100 Reviewers</div>
+          <RiStarFill size={30} />
+          <div className="text-md font-semibold">8.5 Average Scores</div>
           <div className="text-sm font-normal text-gray-600">
-            Attestations from the badgeholders, top 100 delegates, and other
+            Badgeholders and delegates likely to recommend this project to
+            someone else.
           </div>
         </div>
       </div>
